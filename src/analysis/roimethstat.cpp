90d9a4238599718a7c4935072c17ba0c50b35ec5
guilhermesena1 <desenabr@usc.edu>
2022-08-08 11:42:32 -0700

    adding set_show_defaults() so that the user knows which methylation level is being reported by default

--- src/analysis/roimethstat.cpp
+++ src/analysis/roimethstat.cpp
@@ -396,6 +396,7 @@ main_roimethstat(int argc, const char **argv) {
     OptionParser opt_parse(strip_path(argv[0]), "Compute average CpG "
                            "methylation in each of a set of genomic intervals",
                            "<intervals-bed> <methylation-file>");
+    opt_parse.set_show_defaults();
     opt_parse.add_opt("output", 'o', "Name of output file (default: stdout)",
                       false, outfile);
     opt_parse.add_opt("numeric", 'N', "print numeric values only (not NAs)",

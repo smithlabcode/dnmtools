59f3c43c6e74e263acd3d1f624f672ae4ac1120b
Andrew D Smith <andrewds@usc.edu>
2023-09-29 15:21:42 -0700

    Adding dnmtools_utils source files which might be temporary. They are used right now only to do things related to summary stats output from the commands

---
+++ src/common/dnmtools_utils.cpp
@@ -0,0 +1,34 @@
+/* Copyright (C) 2019-2023 Andrew D. Smith
+ *
+ * Authors: Andrew D. Smith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "dnmtools_utils.hpp"
+
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+using std::string;
+
+auto
+get_command_line(const int argc, const char **const argv) -> string {
+  if (argc == 0) return string();
+  std::ostringstream cmd;
+  cmd << '"';
+  std::copy(argv, argv + (argc - 1),
+            std::ostream_iterator<const char *>(cmd, " "));
+  cmd << argv[argc-1] << '"';
+  return cmd.str();
+}
---
+++ src/common/dnmtools_utils.hpp
@@ -0,0 +1,24 @@
+/* Copyright (C) 2019-2023 Andrew D. Smith
+ *
+ * Authors: Andrew D. Smith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef DNMTOOLS_UTILS_HPP
+#define DNMTOOLS_UTILS_HPP
+
+#include <string>
+
+auto
+get_command_line(const int argc, const char **const argv) -> std::string;
+
+#endif

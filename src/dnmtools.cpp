a3574962a84ad86474aa32cc7b002ab78e2ee97d
Andrew D Smith <andrewds@usc.edu>
2025-06-08 22:16:30 -0700

    src/dnmtools.cpp: changed the command for counts for nanopore reads to nanocounts

--- src/dnmtools.cpp
+++ src/dnmtools.cpp
@@ -169,7 +169,7 @@ main(int argc, const char **argv) {
    {"uniq",      "remove duplicate reads from sorted mapped reads",           main_uniq},
    {"bsrate",    "compute the BS conversion rate from BS-seq reads mapped to a genome",  main_bsrate},
    {"counts",    "get methylation levels from mapped WGBS reads",             main_counts},
-   {"nano",      "get methylation levels from mapped nanopore reads",         main_nanocount},
+   {"counts-nano", "get methylation levels from mapped nanopore reads",       main_nanocount},
    {"sym",       "get CpG sites and make methylation levels symmetric",       main_symmetric_cpgs},
    {"levels",    "compute methylation summary statistics from a counts file", main_levels}}}},
 

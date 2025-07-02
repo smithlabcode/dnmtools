edbcc29b781e2e21bc408719f4ddfba859a3c20a
Andrew D Smith <andrewds@usc.edu>
2025-07-02 15:25:14 -0700

    src/common/LevelsCounter.hpp: fixes for static analysis

--- src/common/LevelsCounter.hpp
+++ src/common/LevelsCounter.hpp
@@ -32,37 +32,37 @@ struct LevelsCounter {
   // are consistent with the context string. If the context string is
   // empty or not used, then any constraints on which sites are
   // counted among this total is defined separately.
-  uint64_t total_sites{};
+  std::uint64_t total_sites{};
 
   // sites_covered is the number of reference genome sites analyzed
   // that have at least one mapped read covering them.
-  uint64_t sites_covered{};
+  std::uint64_t sites_covered{};
 
   // max_depth is the number of reads covering the reference genome
   // site having the most mapped reads covering it.
-  uint64_t max_depth{};
+  std::uint64_t max_depth{};
 
   // mutations is the number of sites tagged as having been mutated in
   // the sample based on the mapped reads in comparisin with the
   // underlying reference genome site. This may be indicated in
   // various ways and defined in various ways.
-  uint64_t mutations{};
+  std::uint64_t mutations{};
 
   // total_c is the number of cytosines in mapped reads covering
   // relevant cytosines in the reference genome.
-  uint64_t total_c{};
+  std::uint64_t total_c{};
 
   // total_t is the number of thymines in mapped reads covering
   // relevant cytosines in the reference genome.
-  uint64_t total_t{};
+  std::uint64_t total_t{};
 
   // called_meth is the number of cytosines in the reference genome
   // "called" as being methylated using some statistical criteria.
-  uint64_t called_meth{};
+  std::uint64_t called_meth{};
 
   // called_unmeth is the number of cytosines in the reference genome
   // "called" as being unmethylated using some statistical criteria.
-  uint64_t called_unmeth{};
+  std::uint64_t called_unmeth{};
 
   // total_meth is the sum over all relevant cytosines of the
   // methylation level estimated at that cytosine using the mapped
@@ -72,46 +72,60 @@ struct LevelsCounter {
   // coverage is equal to total_c plus total_t. These are the counts
   // that contribute towards estimates of the various methylation
   // levels.
-  uint64_t coverage() const {return total_c + total_t;}
+  [[nodiscard]] std::uint64_t
+  coverage() const {
+    return total_c + total_t;
+  }
 
   // total_called is equal to called_meth plus called_unmeth
-  uint64_t total_called() const {return called_meth + called_unmeth;}
+  [[nodiscard]] std::uint64_t
+  total_called() const {
+    return called_meth + called_unmeth;
+  }
 
   // mean_meth_weighted is the unweighted mean methylation level. This
   // is the ratio of total_c divided by coverage. This value is always
   // between 0 and 1.
-  double mean_meth_weighted() const {
-    return static_cast<double>(total_c)/
-      std::max(coverage(), static_cast<uint64_t>(1));
+  [[nodiscard]] double
+  mean_meth_weighted() const {
+    return static_cast<double>(total_c) /
+           std::max(coverage(), static_cast<std::uint64_t>(1));
   }
 
   // fractional_meth is the fraction of "called" sites that are called
   // methylated. It is the ratio of called_meth divided by
   // total_called. This value is always between 0 and 1.
-  double fractional_meth() const {
-    return static_cast<double>(called_meth)/
-      std::max(total_called(), static_cast<uint64_t>(1));
+  [[nodiscard]] double
+  fractional_meth() const {
+    return static_cast<double>(called_meth) /
+           std::max(total_called(), static_cast<std::uint64_t>(1));
   }
 
   // mean_meth is the unweighted mean methylation level. This is the
   // ratio of total_meth divided by sites_covered. This value is
   // always between 0 and 1.
-  double mean_meth() const {
-    return static_cast<double>(total_meth)/
-      std::max(sites_covered, static_cast<uint64_t>(1));
+  [[nodiscard]] double
+  mean_meth() const {
+    return static_cast<double>(total_meth) /
+           std::max(sites_covered, static_cast<std::uint64_t>(1));
   }
 
-  LevelsCounter(const std::string &c) : context{c} {}
+  explicit LevelsCounter(const std::string &c) : context{c} {}
 
   LevelsCounter() = default;
 
-  LevelsCounter &operator+=(const LevelsCounter &rhs);
+  LevelsCounter &
+  operator+=(const LevelsCounter &rhs);
 
-  void update(const MSite &s);
+  void
+  update(const MSite &s);
 
-  std::string tostring() const;
-  std::string format_row() const;
-  static std::string format_header();
+  [[nodiscard]] std::string
+  tostring() const;
+  [[nodiscard]] std::string
+  format_row() const;
+  [[nodiscard]] static std::string
+  format_header();
 
   static double alpha;
 };

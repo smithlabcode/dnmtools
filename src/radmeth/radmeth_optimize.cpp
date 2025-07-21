2ac7d8113c8a25b78a539ee9485bebcc9a0375b2
Andrew D Smith <andrewds@usc.edu>
2025-07-20 19:59:17 -0700

    src/radmeth/radmeth_optimize.cpp: major rewrite for speed within the existing approach via general optmizers

--- src/radmeth/radmeth_optimize.cpp
+++ src/radmeth/radmeth_optimize.cpp
@@ -21,179 +21,206 @@
 #include <gsl/gsl_multimin.h>
 #include <gsl/gsl_vector.h>
 
+#include <iomanip>
+#include <iostream>
+#include <numeric>
 #include <stdexcept>
 #include <vector>
 
-using std::vector;
-using std::runtime_error;
-
-static double
-pi(Regression *reg, size_t sample, const gsl_vector *params) {
+[[nodiscard]] static double
+pi(const std::vector<double> &v, const gsl_vector *params) {
   // ADS: this function doesn't have a very helpful name
-  double dot_prod = 0;
-  for (size_t fact = 0; fact < reg->design.n_factors(); ++fact)
-    dot_prod += reg->design.matrix[sample][fact]*gsl_vector_get(params, fact);
-
-  double p = exp(dot_prod)/(1 + exp(dot_prod));
-
-  return p;
+  const auto a = v.data();
+  const double dot = std::inner_product(a, a + std::size(v), params->data, 0.0);
+  // const double p = -1.0 / std::expm1(-dot_prod); ?
+  return std::exp(dot) / (1.0 + std::exp(dot));
 }
 
-
-static double
+[[nodiscard]] static double
 neg_loglik(const gsl_vector *params, void *object) {
   Regression *reg = (Regression *)(object);
-  const size_t n_params = reg->design.factor_names.size() + 1;
-
-  double log_lik = 0;
 
   // ADS: the dispersion parameter phi is the last element of
   // parameter vector
-  const double disp_param = gsl_vector_get(params, n_params - 1);
-  const double phi = exp(disp_param)/(1.0 + exp(disp_param));
+  const double disp_param = gsl_vector_get(params, reg->design.n_factors());
+  const double phi = std::exp(disp_param) / (1.0 + std::exp(disp_param));
+  const double one_minus_phi = 1.0 - phi;
 
-  for(size_t s = 0; s < reg->design.n_samples(); ++s) {
-    const double n_s = reg->props.total[s];
-    const double y_s = reg->props.meth[s];
-    const double p_s = pi(reg, s, params);
+  const auto &mc = reg->props.mc;
 
-    for (int k = 0; k < y_s; ++k)
-      log_lik += log((1 - phi)*p_s + phi*k);
+  double log_lik = 0;
+  const std::size_t n_samples = reg->design.n_samples();
+  for (std::size_t col_idx = 0; col_idx < n_samples; ++col_idx) {
+    const double n = mc[col_idx].n_reads;
+    const double y = mc[col_idx].n_meth;
+    const double p = pi(reg->design.matrix[col_idx], params);
+    const double one_minus_p = 1.0 - p;
 
-    for (int k = 0; k < n_s - y_s; ++k)
-      log_lik += log((1 - phi)*(1 - p_s) + phi*k);
+    const double term1 = one_minus_phi * p;
+    for (int k = 0; k < y; ++k)
+      log_lik += std::log(term1 + phi * k);
 
-    for (int k = 0; k < n_s; ++k)
-      log_lik -= log(1.0 + phi*(k - 1));
+    const double term2 = one_minus_phi * one_minus_p;
+    for (int k = 0; k < n - y; ++k)
+      log_lik += std::log(term2 + phi * k);
+
+    for (int k = 0; k < n; ++k)
+      log_lik -= std::log(1.0 + phi * (k - 1.0));
   }
-
   return -log_lik;
 }
 
-
 static void
-neg_gradient(const gsl_vector *params, void *object,
-             gsl_vector *output) {
-
+neg_gradient(const gsl_vector *params, void *object, gsl_vector *output) {
   Regression *reg = (Regression *)(object);
-  const size_t n_params = reg->design.n_factors() + 1;
+  const std::size_t n_samples = reg->design.n_samples();
+  const std::size_t n_factors = reg->design.n_factors();
 
-  const double disp_param = gsl_vector_get(params, n_params - 1);
+  std::vector<double> p_v(n_samples, 0.0);
+  for (auto col_idx = 0u; col_idx < n_samples; ++col_idx)
+    p_v[col_idx] = pi(reg->design.matrix[col_idx], params);
 
-  const double phi = exp(disp_param)/(1 + exp(disp_param));
+  const double disp_param = gsl_vector_get(params, n_factors);
 
-  for(size_t f = 0; f < n_params; ++f) {
+  const auto &tmatrix = reg->design.tmatrix;
+  const double phi = std::exp(disp_param) / (1.0 + std::exp(disp_param));
+  // const double phi = -1.0 / std::expm1(-disp_param);
+  const double one_minus_phi = 1.0 - phi;
 
+  const auto &mc = reg->props.mc;
+
+  for (std::size_t f = 0; f < n_factors; ++f) {
     double deriv = 0;
+    const auto &vec = tmatrix[f];
+    for (std::size_t col_idx = 0; col_idx < n_samples; ++col_idx) {
+      const double n = mc[col_idx].n_reads;
+      const double y = mc[col_idx].n_meth;
+      const double p = p_v[col_idx];
+      const double one_minus_p = 1.0 - p;
 
-    for(size_t s = 0; s < reg->design.n_samples(); ++s) {
-      int n_s = reg->props.total[s];
-      int y_s = reg->props.meth[s];
-      double p_s = pi(reg, s, params);
+      const double denom_term1 = one_minus_phi * p;
+      const double factor = denom_term1 * one_minus_p * vec[col_idx];
+      if (factor == 0)
+        continue;
 
       double term = 0;
 
-      //a parameter linked to p
-      if(f < reg->design.factor_names.size()) {
-        double factor = (1 - phi)*p_s*(1 - p_s)*reg->design.matrix[s][f];
-        if (factor == 0) continue;
+      const auto accumulate_term = [&](int k, const int lim,
+                                       const double denom_base) {
+        for (; k < lim; ++k)
+          term += 1.0 / (denom_base + phi * k);
+      };
 
-        for(int k = 0; k < y_s; ++k)
-          term += 1/((1 - phi)*p_s + phi*k);
+      const auto accumulate_subtract_term = [&](int k, const int lim,
+                                                const double denom_base) {
+        for (; k < lim; ++k)
+          term -= 1.0 / (denom_base + phi * k);
+      };
 
-        for(int k = 0; k < n_s - y_s; ++k)
-          term -= 1/((1 - phi)*(1 - p_s) + phi*k);
+      accumulate_term(0, y, denom_term1);
+      accumulate_subtract_term(0, n - y, one_minus_phi * one_minus_p);
 
-        deriv += term*factor;
-      } else { // the parameter linked to phi
-        for(int k = 0; k < y_s; ++k)
-          term += (k - p_s)/((1 - phi)*p_s + phi*k);
-
-        for(int k = 0; k < n_s - y_s; ++k)
-          term += (k - (1 - p_s))/((1 - phi)*(1 - p_s) + phi*k);
-
-        for(int k = 0; k < n_s; ++k) {
-          term -= (k - 1)/(1 + phi*(k - 1));
-        }
-
-        deriv += term * phi * (1 - phi);
-      }
+      deriv += term * factor;
     }
-
     gsl_vector_set(output, f, deriv);
   }
 
+  double deriv = 0;
+  for (std::size_t col_idx = 0; col_idx < n_samples; ++col_idx) {
+    const double n = mc[col_idx].n_reads;
+    const double y = mc[col_idx].n_meth;
+    const double p = p_v[col_idx];
+    const double one_minus_p = 1.0 - p;
+
+    double term = 0;
+    const auto accumulate_term = [&](int k, const int lim,
+                                     const double num_shift,
+                                     const double denom_base) {
+      for (; k < lim; ++k)
+        term += (k - num_shift) / (denom_base + phi * k);
+    };
+    const auto accumulate_subtract_term = [&](int k, const int lim) {
+      for (; k < lim; ++k)
+        term -= (k - 1.0) / (1.0 + phi * (k - 1.0));
+    };
+
+    accumulate_term(0, y, p, one_minus_phi * p);
+    accumulate_term(0, n - y, one_minus_p, one_minus_phi * one_minus_p);
+    accumulate_subtract_term(0, n);
+
+    deriv += term * phi * one_minus_phi;
+  }
+  gsl_vector_set(output, n_factors, deriv);
   gsl_vector_scale(output, -1.0);
 }
 
 static void
-neg_loglik_and_grad(const gsl_vector *params,
-                    void *object,
-                    double *loglik_val,
+neg_loglik_and_grad(const gsl_vector *params, void *object, double *loglik_val,
                     gsl_vector *d_loglik_val) {
-
   *loglik_val = neg_loglik(params, object);
   neg_gradient(params, object, d_loglik_val);
 }
 
-
 bool
-fit_regression_model(Regression &r, vector<double> &initial_params) {
+fit_regression_model(Regression &r, std::vector<double> &params_init) {
+  const auto tolerance = Regression::tolerance;
+  const auto stepsize = Regression::stepsize;
+  const auto max_iter = Regression::max_iter;
 
   // one more than the number of factors
-  const size_t n_params = r.n_factors() + 1;
-  if (initial_params.empty()) {
-    initial_params.resize(n_params, 0.0);
-    initial_params.back() = -2.5;
+  const std::size_t n_params = r.n_factors() + 1;
+  if (params_init.empty()) {
+    params_init.resize(n_params, 0.0);
+    params_init.back() = -2.5;
   }
 
-  if (initial_params.size() != n_params)
-    throw runtime_error("Wrong number of initial parameters.");
+  if (params_init.size() != n_params)
+    throw std::runtime_error("Wrong number of initial parameters.");
+
+  // clang-format off
+  gsl_multimin_function_fdf loglik_bundle = {
+    &neg_loglik,              // objective function
+    &neg_gradient,            // gradient
+    &neg_loglik_and_grad,     // combined obj and grad
+    n_params,                 // number of model params
+    static_cast<void *>(&r)   // parameters for objective and gradient functions
+  };
+  // clang-format on
+
+  // Alternatives:
+  // - gsl_multimin_fdfminimizer_conjugate_pr
+  // - gsl_multimin_fdfminimizer_conjugate_fr
+  // - gsl_multimin_fdfminimizer_vector_bfgs2
+  // - gsl_multimin_fdfminimizer_steepest_descent
+  const gsl_multimin_fdfminimizer_type *minimizer =
+    gsl_multimin_fdfminimizer_conjugate_fr;
+
+  gsl_multimin_fdfminimizer *s =
+    gsl_multimin_fdfminimizer_alloc(minimizer, n_params);
+
+  gsl_vector *params = gsl_vector_alloc(n_params);
+  for (std::size_t i = 0; i < n_params; ++i)
+    gsl_vector_set(params, i, params_init[i]);
+
+  gsl_multimin_fdfminimizer_set(s, &loglik_bundle, params, stepsize, tolerance);
 
   int status = 0;
+  std::size_t iter = 0;
 
-  size_t iter = 0;
+  do {
+    status = gsl_multimin_fdfminimizer_iterate(s);  // one iter and get status
+    if (status)
+      break;
+    // check status from gradient
+    status = gsl_multimin_test_gradient(s->gradient, tolerance);
+  } while (status == GSL_CONTINUE && ++iter < max_iter);
+  // It it reasonable to reduce the number of iterations to 500?
+  // ADS: 700 vs. 500? what's the difference?
 
-  {
-    gsl_multimin_function_fdf loglik_bundle;
-    loglik_bundle.f = &neg_loglik;
-    loglik_bundle.df = &neg_gradient;
-    loglik_bundle.fdf = &neg_loglik_and_grad;
-    loglik_bundle.n = n_params;
-    loglik_bundle.params = (void *)&r;
+  r.max_loglik = -1.0 * neg_loglik(s->x, &r);
 
-    gsl_vector *params = gsl_vector_alloc(n_params);
-
-    for (size_t param = 0; param < initial_params.size(); ++param)
-      gsl_vector_set(params, param, initial_params[param]);
-
-    //can also try gsl_multimin_fdfminimizer_conjugate_pr;
-    const gsl_multimin_fdfminimizer_type *T = gsl_multimin_fdfminimizer_conjugate_fr;
-
-    gsl_multimin_fdfminimizer *s = gsl_multimin_fdfminimizer_alloc (T, n_params);
-
-    // ADS: 0.001 is the step size and 1e-4 is the tolerance
-    // get the minimizer
-    gsl_multimin_fdfminimizer_set(s, &loglik_bundle, params, 0.001, 1e-4);
-
-    do {
-      iter++;
-      // do one iteration and get the status
-      status = gsl_multimin_fdfminimizer_iterate(s);
-      if (status)
-        break;
-      // check the status based on gradient
-      status = gsl_multimin_test_gradient (s->gradient, 1e-4);
-    }
-    while (status == GSL_CONTINUE && iter < 700);
-    // It it reasonable to reduce the number of iterations to 500?
-    // ADS: 700 vs. 500? what's the difference?
-
-    r.max_loglik = (-1)*neg_loglik(s->x, &r);
-
-    gsl_multimin_fdfminimizer_free(s);
-    gsl_vector_free(params);
-  }
+  gsl_multimin_fdfminimizer_free(s);
+  gsl_vector_free(params);
 
   return status == GSL_SUCCESS;
 }

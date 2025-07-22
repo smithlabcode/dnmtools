354c3436b7aa5e69ca4de7bdcb4ab97f82b4256a
Andrew D Smith <andrewds@usc.edu>
2025-07-22 15:46:48 -0700

    src/radmeth/radmeth_optimize.cpp: slight changes to some numerical calculations and modifying the p_v in neg_gradient to be held in Regression to avoid reallocating

--- src/radmeth/radmeth_optimize.cpp
+++ src/radmeth/radmeth_optimize.cpp
@@ -35,8 +35,8 @@ pi(const std::vector<T> &v, const gsl_vector *params) {
   // ADS: this function doesn't have a very helpful name
   const auto a = v.data();
   const double dot = std::inner_product(a, a + std::size(v), params->data, 0.0);
-  // const double p = -1.0 / std::expm1(-dot_prod); ?
-  return std::exp(dot) / (1.0 + std::exp(dot));
+  // const double p = std::exp(dot) / (1.0 + std::exp(dot));
+  return 1.0 / (1.0 / std::exp(dot) + 1.0);
 }
 
 [[nodiscard]] static double
@@ -46,29 +46,32 @@ neg_loglik(const gsl_vector *params, void *object) {
   // ADS: the dispersion parameter phi is the last element of
   // parameter vector
   const double disp_param = gsl_vector_get(params, reg->design.n_factors());
-  const double phi = std::exp(disp_param) / (1.0 + std::exp(disp_param));
+  // const double phi = std::exp(disp_param) / (1.0 + std::exp(disp_param));
+  const double phi = 1.0 / (1.0 / std::exp(disp_param) + 1.0);
   const double one_minus_phi = 1.0 - phi;
 
   const auto &mc = reg->props.mc;
+  const auto &mat = reg->design.matrix;
 
   double log_lik = 0;
   const std::size_t n_samples = reg->design.n_samples();
   for (std::size_t col_idx = 0; col_idx < n_samples; ++col_idx) {
-    const double n = mc[col_idx].n_reads;
-    const double y = mc[col_idx].n_meth;
-    const double p = pi(reg->design.matrix[col_idx], params);
+    const auto n = mc[col_idx].n_reads;
+    const auto y = mc[col_idx].n_meth;
+    const double p = pi(mat[col_idx], params);
     const double one_minus_p = 1.0 - p;
 
     const double term1 = one_minus_phi * p;
-    for (int k = 0; k < y; ++k)
+    for (auto k = 0u; k < y; ++k)
       log_lik += std::log(term1 + phi * k);
 
     const double term2 = one_minus_phi * one_minus_p;
-    for (int k = 0; k < n - y; ++k)
+    for (auto k = 0u; k < n - y; ++k)
       log_lik += std::log(term2 + phi * k);
 
-    for (int k = 0; k < n; ++k)
-      log_lik -= std::log(1.0 + phi * (k - 1.0));
+    for (auto k = 0u; k < n; ++k)
+      // log_lik -= std::log(1.0 + phi * (k - 1.0));
+      log_lik -= std::log1p(phi * (k - 1.0));
   }
   return -log_lik;
 }
@@ -79,25 +82,27 @@ neg_gradient(const gsl_vector *params, void *object, gsl_vector *output) {
   const std::size_t n_samples = reg->design.n_samples();
   const std::size_t n_factors = reg->design.n_factors();
 
-  std::vector<double> p_v(n_samples, 0.0);
+  /// ADS: using scratch space held in reg instead of allocating here
+  // std::vector<double> p_v(n_samples, 0.0);
+  auto &p_v = reg->p_v;
   for (auto col_idx = 0u; col_idx < n_samples; ++col_idx)
     p_v[col_idx] = pi(reg->design.matrix[col_idx], params);
 
   const double disp_param = gsl_vector_get(params, n_factors);
 
-  const auto &tmatrix = reg->design.tmatrix;
-  const double phi = std::exp(disp_param) / (1.0 + std::exp(disp_param));
-  // const double phi = -1.0 / std::expm1(-disp_param);
+  const auto &tmat = reg->design.tmatrix;
+  // const double phi = std::exp(disp_param) / (1.0 + std::exp(disp_param));
+  const double phi = 1.0 / (1.0 / std::exp(disp_param) + 1.0);
   const double one_minus_phi = 1.0 - phi;
 
   const auto &mc = reg->props.mc;
 
-  for (std::size_t f = 0; f < n_factors; ++f) {
+  for (std::size_t factor_idx = 0; factor_idx < n_factors; ++factor_idx) {
     double deriv = 0;
-    const auto &vec = tmatrix[f];
+    const auto &vec = tmat[factor_idx];
     for (std::size_t col_idx = 0; col_idx < n_samples; ++col_idx) {
-      const double n = mc[col_idx].n_reads;
-      const double y = mc[col_idx].n_meth;
+      const auto n = mc[col_idx].n_reads;
+      const auto y = mc[col_idx].n_meth;
       const double p = p_v[col_idx];
       const double one_minus_p = 1.0 - p;
 
@@ -108,48 +113,47 @@ neg_gradient(const gsl_vector *params, void *object, gsl_vector *output) {
 
       double term = 0;
 
-      const auto accumulate_term = [&](int k, const int lim,
-                                       const double denom_base) {
+      const auto accum_term = [&](auto k, const auto lim,
+                                  const double denom_base) {
         for (; k < lim; ++k)
           term += 1.0 / (denom_base + phi * k);
       };
 
-      const auto accumulate_subtract_term = [&](int k, const int lim,
-                                                const double denom_base) {
+      const auto accum_subtract_term = [&](auto k, const auto lim,
+                                           const double denom_base) {
         for (; k < lim; ++k)
           term -= 1.0 / (denom_base + phi * k);
       };
 
-      accumulate_term(0, y, denom_term1);
-      accumulate_subtract_term(0, n - y, one_minus_phi * one_minus_p);
+      accum_term(0u, y, denom_term1);
+      accum_subtract_term(0u, n - y, one_minus_phi * one_minus_p);
 
       deriv += term * factor;
     }
-    gsl_vector_set(output, f, deriv);
+    gsl_vector_set(output, factor_idx, deriv);
   }
 
   double deriv = 0;
   for (std::size_t col_idx = 0; col_idx < n_samples; ++col_idx) {
-    const double n = mc[col_idx].n_reads;
-    const double y = mc[col_idx].n_meth;
+    const auto n = mc[col_idx].n_reads;
+    const auto y = mc[col_idx].n_meth;
     const double p = p_v[col_idx];
     const double one_minus_p = 1.0 - p;
 
     double term = 0;
-    const auto accumulate_term = [&](int k, const int lim,
-                                     const double num_shift,
-                                     const double denom_base) {
+    const auto accum_term = [&](auto k, const auto lim, const double num_shift,
+                                const double denom_base) {
       for (; k < lim; ++k)
         term += (k - num_shift) / (denom_base + phi * k);
     };
-    const auto accumulate_subtract_term = [&](int k, const int lim) {
+    const auto accum_subtract_term = [&](auto k, const auto lim) {
       for (; k < lim; ++k)
         term -= (k - 1.0) / (1.0 + phi * (k - 1.0));
     };
 
-    accumulate_term(0, y, p, one_minus_phi * p);
-    accumulate_term(0, n - y, one_minus_p, one_minus_phi * one_minus_p);
-    accumulate_subtract_term(0, n);
+    accum_term(0u, y, p, one_minus_phi * p);
+    accum_term(0u, n - y, one_minus_p, one_minus_phi * one_minus_p);
+    accum_subtract_term(0u, n);
 
     deriv += term * phi * one_minus_phi;
   }
@@ -175,6 +179,8 @@ fit_regression_model(Regression &r, std::vector<double> &params_init) {
     params_init.resize(n_params, 0.0);
     params_init.back() = -2.5;
   }
+
+  r.p_v.resize(r.n_samples());
 
   const double tolerance =
     std::sqrt(n_params) * r.n_samples() * Regression::tolerance;
@@ -219,8 +225,7 @@ fit_regression_model(Regression &r, std::vector<double> &params_init) {
     // check status from gradient
     status = gsl_multimin_test_gradient(s->gradient, tolerance);
   } while (status == GSL_CONTINUE && ++iter < max_iter);
-  // It it reasonable to reduce the number of iterations to 500?
-  // ADS: 700 vs. 500? what's the difference?
+  // ADS: What is a reasonable number of iterations?
 
   r.max_loglik = -1.0 * neg_loglik(s->x, &r);
 

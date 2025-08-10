a9631fe3d54988f5caecb750d301be2701046b1c
Andrew D Smith <andrewds@usc.edu>
2025-08-09 21:15:52 -0700

    src/radmeth/radmeth_optimize.cpp: cleaning up some code and tweaking for more optimization

--- src/radmeth/radmeth_optimize.cpp
+++ src/radmeth/radmeth_optimize.cpp
@@ -16,7 +16,6 @@
  */
 
 #include "radmeth_optimize.hpp"
-
 #include "radmeth_model.hpp"
 
 #include <gsl/gsl_multimin.h>
@@ -25,9 +24,6 @@
 #include <numeric>
 #include <stdexcept>
 #include <vector>
-
-#include <iomanip>
-#include <iostream>
 
 [[nodiscard]] static inline double
 logistic(const double x) {
@@ -42,29 +38,25 @@ get_p(const std::vector<T> &v, const gsl_vector *params) {
 }
 
 [[nodiscard]] static double
-neg_loglik(const gsl_vector *params, void *object) {
-  Regression *reg = (Regression *)(object);
+log_likelihood(const gsl_vector *params, const Regression &reg) {
+  const auto phi = logistic(gsl_vector_get(params, reg.design.n_factors()));
+  const auto one_minus_phi = 1.0 - phi;
 
-  // ADS: dispersion param phi is the last element of parameter vector
-  const double disp_param = gsl_vector_get(params, reg->design.n_factors());
-  const double phi = logistic(disp_param);
-  const double one_minus_phi = 1.0 - phi;
+  const auto n_groups = reg.n_groups();
+  const auto &groups = reg.design.groups;
+  const auto &cumul = reg.cumul;
 
-  const auto n_groups = reg->n_groups();
-  const auto &groups = reg->design.groups;
-  const auto &cumul = reg->cumul;
-
-  double log_lik = 0;
+  double log_lik = 0.0;
   for (std::size_t g_idx = 0; g_idx < n_groups; ++g_idx) {
-    const double p = get_p(groups[g_idx], params);
-    const double one_minus_p = 1.0 - p;
+    const auto p = get_p(groups[g_idx], params);
+    const auto one_minus_p = 1.0 - p;
 
-    const double term1 = one_minus_phi * p;
+    const auto term1 = one_minus_phi * p;
     const auto &cumul_y = cumul[g_idx].m_counts;
     for (std::size_t k = 0; k < std::size(cumul_y); ++k)
       log_lik += cumul_y[k] * std::log(term1 + phi * k);
 
-    const double term2 = one_minus_phi * one_minus_p;
+    const auto term2 = one_minus_phi * one_minus_p;
     const auto &cumul_d = cumul[g_idx].d_counts;
     for (std::size_t k = 0; k < std::size(cumul_d); ++k)
       log_lik += cumul_d[k] * std::log(term2 + phi * k);
@@ -73,83 +65,88 @@ neg_loglik(const gsl_vector *params, void *object) {
     for (std::size_t k = 0; k < std::size(cumul_n); ++k)
       log_lik -= cumul_n[k] * std::log1p(phi * (k - 1.0));
   }
+  return log_lik;
+}
 
-  return -log_lik;
+static void
+gradient(const gsl_vector *params, Regression &reg, gsl_vector *output) {
+  const auto n_factors = reg.design.n_factors();
+  const auto phi = logistic(gsl_vector_get(params, n_factors));
+  const auto one_minus_phi = 1.0 - phi;
+
+  const auto n_groups = reg.n_groups();
+  const auto &groups = reg.design.groups;
+  const auto &cumul = reg.cumul;
+
+  auto &p_v = reg.p_v;  // ADS: reusing scratch space
+  for (auto g_idx = 0u; g_idx < n_groups; ++g_idx)
+    p_v[g_idx] = get_p(groups[g_idx], params);
+
+  // init output to zero for all factors
+  gsl_vector_set_all(output, 0.0);
+  auto &data = output->data;
+
+  for (std::size_t g_idx = 0; g_idx < n_groups; ++g_idx) {
+    const auto p = p_v[g_idx];
+    const auto one_minus_p = 1.0 - p;
+
+    double deriv = 0.0;
+
+    const auto denom_term1 = one_minus_phi * p;
+    const auto &cumul_y = cumul[g_idx].m_counts;
+    for (auto k = 0u; k < std::size(cumul_y); ++k)
+      deriv += cumul_y[k] / (denom_term1 + phi * k);
+
+    const auto denom_term2 = one_minus_phi * one_minus_p;
+    const auto &cumul_d = cumul[g_idx].d_counts;
+    for (auto k = 0u; k < std::size(cumul_d); ++k)
+      deriv -= cumul_d[k] / (denom_term2 + phi * k);
+
+    const auto &g = groups[g_idx];
+    for (auto fact_idx = 0u; fact_idx < n_factors; ++fact_idx) {
+      const auto level = g[fact_idx];
+      if (level == 0)
+        continue;
+      data[fact_idx] += deriv * (denom_term1 * one_minus_p * level);
+    }
+  }
+
+  double deriv = 0.0;
+  auto cumul_itr = std::cbegin(cumul);
+  for (auto g_idx = 0u; g_idx < n_groups; ++g_idx, ++cumul_itr) {
+    const auto p = get_p(groups[g_idx], params);
+    const auto one_minus_p = 1.0 - p;
+
+    const auto term1 = one_minus_phi * p;
+    const auto &cumul_y = cumul_itr->m_counts;
+    const auto y_lim = std::size(cumul_y);
+    for (auto k = 0u; k < y_lim; ++k)
+      deriv += cumul_y[k] * (k - p) / (term1 + phi * k);
+
+    const auto term2 = one_minus_phi * one_minus_p;
+    const auto &cumul_d = cumul_itr->d_counts;
+    const auto d_lim = std::size(cumul_d);
+    for (auto k = 0u; k < d_lim; ++k)
+      deriv += cumul_d[k] * (k - one_minus_p) / (term2 + phi * k);
+
+    const auto &cumul_n = cumul_itr->r_counts;
+    const auto n_lim = std::size(cumul_n);
+    for (auto k = 0u; k < n_lim; ++k)
+      deriv -= cumul_n[k] * (k - 1.0) / (1.0 + phi * (k - 1.0));
+  }
+  gsl_vector_set(output, n_factors, deriv * (phi * one_minus_phi));
+}
+
+[[nodiscard]] static double
+neg_loglik(const gsl_vector *params, void *object) {
+  auto reg = static_cast<Regression *>(object);
+  return -log_likelihood(params, *reg);
 }
 
 static void
 neg_gradient(const gsl_vector *params, void *object, gsl_vector *output) {
-  Regression *reg = (Regression *)(object);
-
-  const auto n_groups = reg->n_groups();
-  const auto &groups = reg->design.groups;
-  const auto &cumul = reg->cumul;
-
-  // ADS: using scratch space held in reg instead of allocating here
-  auto &p_v = reg->p_v;
-  for (auto g_idx = 0u; g_idx < n_groups; ++g_idx)
-    p_v[g_idx] = get_p(groups[g_idx], params);
-
-  const std::size_t n_factors = reg->design.n_factors();
-  const double disp_param = gsl_vector_get(params, n_factors);
-
-  const double phi = logistic(disp_param);
-  const double one_minus_phi = 1.0 - phi;
-
-  // init output to zero for all factors
-  gsl_vector_set_all(output, 0.0);
-
-  for (std::size_t g_idx = 0; g_idx < n_groups; ++g_idx) {
-    const double p = p_v[g_idx];
-    const double one_minus_p = 1.0 - p;
-    const double denom_term1 = one_minus_phi * p;
-    const double denom_term2 = one_minus_phi * one_minus_p;
-
-    double term = 0;
-
-    const auto &cumul_y = cumul[g_idx].m_counts;
-    for (auto k = 0u; k < std::size(cumul_y); ++k)
-      term += cumul_y[k] / (denom_term1 + phi * k);
-
-    const auto &cumul_d = cumul[g_idx].d_counts;
-    for (auto k = 0u; k < std::size(cumul_d); ++k)
-      term -= cumul_d[k] / (denom_term2 + phi * k);
-
-    const auto &g = groups[g_idx];
-
-    for (std::size_t factor_idx = 0; factor_idx < n_factors; ++factor_idx) {
-      const auto level = g[factor_idx];
-      if (level == 0)
-        continue;
-      const double factor = denom_term1 * one_minus_p * level;
-      double deriv = gsl_vector_get(output, factor_idx);
-      deriv += term * factor;
-      gsl_vector_set(output, factor_idx, deriv);
-    }
-  }
-
-  double deriv = 0;
-  for (std::size_t g_idx = 0; g_idx < n_groups; ++g_idx) {
-    const double p = get_p(groups[g_idx], params);
-    const double one_minus_p = 1.0 - p;
-
-    const double term1 = one_minus_phi * p;
-    const auto &cumul_y = cumul[g_idx].m_counts;
-    for (auto k = 0u; k < std::size(cumul_y); ++k)
-      deriv += cumul_y[k] * (k - p) / (term1 + phi * k);
-
-    const double term2 = one_minus_phi * one_minus_p;
-    const auto &cumul_d = cumul[g_idx].d_counts;
-    for (auto k = 0u; k < std::size(cumul_d); ++k)
-      deriv += cumul_d[k] * (k - one_minus_p) / (term2 + phi * k);
-
-    const auto &cumul_n = cumul[g_idx].r_counts;
-    for (std::size_t k = 0; k < std::size(cumul_n); ++k)
-      deriv -= cumul_n[k] * (k - 1.0) / (1.0 + phi * (k - 1.0));
-  }
-  deriv *= (phi * one_minus_phi);
-
-  gsl_vector_set(output, n_factors, deriv);
+  auto reg = static_cast<Regression *>(object);
+  gradient(params, *reg, output);
   gsl_vector_scale(output, -1.0);
 }
 
@@ -163,12 +160,12 @@ neg_loglik_and_grad(const gsl_vector *params, void *object, double *loglik_val,
 static void
 get_cumulative(const std::vector<std::uint32_t> &group_id,
                const std::uint32_t n_groups, const std::vector<mcounts> &mc,
-               std::vector<cumulative_counts> &cumul) {
+               std::vector<cumul_counts> &cumul) {
   const auto n_cols = std::size(mc);
   cumul.clear();
   cumul.resize(n_groups);
 
-  const auto compute_cumulative = [&](auto get_value, auto get_vector) {
+  const auto comp_cumul = [&](auto get_value, auto get_vector) {
     // phase 1: determine max value for each group
     for (auto g_idx = 0u; g_idx < n_groups; ++g_idx) {
       std::uint32_t max_v{};
@@ -192,24 +189,22 @@ get_cumulative(const std::vector<std::uint32_t> &group_id,
     }
   };
   // call the lambda 3 times for m_counts, r_counts, d_counts
-  compute_cumulative([](const mcounts &m) { return m.n_meth; },
-                     [](cumulative_counts &c) -> std::vector<std::uint32_t> & {
-                       return c.m_counts;
-                     });
+  comp_cumul(
+    [](const mcounts &m) { return m.n_meth; },
+    [](cumul_counts &c) -> std::vector<std::uint32_t> & { return c.m_counts; });
 
-  compute_cumulative([](const mcounts &m) { return m.n_reads; },
-                     [](cumulative_counts &c) -> std::vector<std::uint32_t> & {
-                       return c.r_counts;
-                     });
+  comp_cumul(
+    [](const mcounts &m) { return m.n_reads; },
+    [](cumul_counts &c) -> std::vector<std::uint32_t> & { return c.r_counts; });
 
-  compute_cumulative([](const mcounts &m) { return m.n_reads - m.n_meth; },
-                     [](cumulative_counts &c) -> std::vector<std::uint32_t> & {
-                       return c.d_counts;
-                     });
+  comp_cumul(
+    [](const mcounts &m) { return m.n_reads - m.n_meth; },
+    [](cumul_counts &c) -> std::vector<std::uint32_t> & { return c.d_counts; });
 }
 
 [[nodiscard]] bool
 fit_regression_model(Regression &r, std::vector<double> &params_init) {
+  static constexpr auto init_dispersion_param = -2.5;
   const auto stepsize = Regression::stepsize;
   const auto max_iter = Regression::max_iter;
 
@@ -219,19 +214,14 @@ fit_regression_model(Regression &r, std::vector<double> &params_init) {
   const std::size_t n_params = r.n_factors() + 1;
   if (params_init.empty()) {
     params_init.resize(n_params, 0.0);
-    params_init.back() = -2.5;
+    params_init.back() = init_dispersion_param;
   }
-
-  r.p_v.resize(r.n_groups());
-
-  const double tolerance =
-    std::sqrt(n_params) * r.n_samples() * Regression::tolerance;
-
-  if (params_init.size() != n_params)
+  if (std::size(params_init) != n_params)
     throw std::runtime_error("Wrong number of initial parameters.");
-
+  r.p_v.resize(r.n_groups());
+  const auto tol = std::sqrt(n_params) * r.n_samples() * Regression::tolerance;
   // clang-format off
-  gsl_multimin_function_fdf loglik_bundle = {
+  auto loglik_bundle = gsl_multimin_function_fdf{
     &neg_loglik,              // objective function
     &neg_gradient,            // gradient
     &neg_loglik_and_grad,     // combined obj and grad
@@ -245,30 +235,27 @@ fit_regression_model(Regression &r, std::vector<double> &params_init) {
   // - gsl_multimin_fdfminimizer_conjugate_fr
   // - gsl_multimin_fdfminimizer_vector_bfgs2
   // - gsl_multimin_fdfminimizer_steepest_descent
-  const gsl_multimin_fdfminimizer_type *minimizer =
-    gsl_multimin_fdfminimizer_conjugate_pr;
+  const auto minimizer = gsl_multimin_fdfminimizer_conjugate_pr;
+  auto s = gsl_multimin_fdfminimizer_alloc(minimizer, n_params);
 
-  gsl_multimin_fdfminimizer *s =
-    gsl_multimin_fdfminimizer_alloc(minimizer, n_params);
-
-  gsl_vector *params = gsl_vector_alloc(n_params);
-  for (std::size_t i = 0; i < n_params; ++i)
+  auto params = gsl_vector_alloc(n_params);
+  for (auto i = 0u; i < n_params; ++i)
     gsl_vector_set(params, i, params_init[i]);
 
-  gsl_multimin_fdfminimizer_set(s, &loglik_bundle, params, stepsize, tolerance);
+  gsl_multimin_fdfminimizer_set(s, &loglik_bundle, params, stepsize, tol);
 
   int status = 0;
   std::size_t iter = 0;
-
   do {
     status = gsl_multimin_fdfminimizer_iterate(s);  // one iter and get status
     if (status)
       break;
     // check status from gradient
-    status = gsl_multimin_test_gradient(s->gradient, tolerance);
+    status = gsl_multimin_test_gradient(s->gradient, tol);
   } while (status == GSL_CONTINUE && ++iter < max_iter);
 
-  r.max_loglik = -1.0 * neg_loglik(s->x, &r);
+  const auto param_estimates = gsl_multimin_fdfminimizer_x(s);
+  r.max_loglik = log_likelihood(param_estimates, r);
 
   gsl_multimin_fdfminimizer_free(s);
   gsl_vector_free(params);

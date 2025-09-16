e38cc02a5baec64e3349fdb76518aade057dfb66
Andrew D Smith <andrewds@usc.edu>
2025-09-15 17:43:59 -0700

    src/common/LevelsCounter.hpp: just moving some things around

--- src/common/LevelsCounter.hpp
+++ src/common/LevelsCounter.hpp
@@ -2,15 +2,15 @@
  *
  * Authors: Andrew D. Smith
  *
- * This is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
  * This software is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
  */
 
 #ifndef LEVELS_COUNTER_HPP
@@ -28,50 +28,49 @@ struct LevelsCounter {
   // tools provide more information about this context string.
   std::string context;
 
-  // total_sites is the total number of sites summarized, all of which
-  // are consistent with the context string. If the context string is
-  // empty or not used, then any constraints on which sites are
-  // counted among this total is defined separately.
+  // total_sites is the total number of sites summarized, all of which are
+  // consistent with the context string. If the context string is empty or not
+  // used, then any constraints on which sites are counted among this total is
+  // defined separately.
   std::uint64_t total_sites{};
 
   // sites_covered is the number of reference genome sites analyzed
   // that have at least one mapped read covering them.
   std::uint64_t sites_covered{};
 
-  // max_depth is the number of reads covering the reference genome
-  // site having the most mapped reads covering it.
+  // max_depth is the number of reads covering the reference genome site
+  // having the most mapped reads covering it.
   std::uint64_t max_depth{};
 
-  // mutations is the number of sites tagged as having been mutated in
-  // the sample based on the mapped reads in comparisin with the
-  // underlying reference genome site. This may be indicated in
-  // various ways and defined in various ways.
+  // mutations is the number of sites tagged as having been mutated in the
+  // sample based on the mapped reads in comparisin with the underlying
+  // reference genome site. This may be indicated in various ways and defined
+  // in various ways.
   std::uint64_t mutations{};
 
-  // total_c is the number of cytosines in mapped reads covering
-  // relevant cytosines in the reference genome.
+  // total_c is the number of cytosines in mapped reads covering relevant
+  // cytosines in the reference genome.
   std::uint64_t total_c{};
 
-  // total_t is the number of thymines in mapped reads covering
-  // relevant cytosines in the reference genome.
+  // total_t is the number of thymines in mapped reads covering relevant
+  // cytosines in the reference genome.
   std::uint64_t total_t{};
 
-  // called_meth is the number of cytosines in the reference genome
-  // "called" as being methylated using some statistical criteria.
+  // called_meth is the number of cytosines in the reference genome "called"
+  // as being methylated using some statistical criteria.
   std::uint64_t called_meth{};
 
-  // called_unmeth is the number of cytosines in the reference genome
-  // "called" as being unmethylated using some statistical criteria.
+  // called_unmeth is the number of cytosines in the reference genome "called"
+  // as being unmethylated using some statistical criteria.
   std::uint64_t called_unmeth{};
 
-  // total_meth is the sum over all relevant cytosines of the
-  // methylation level estimated at that cytosine using the mapped
-  // reads. This value contributes to the unweighted mean methylation.
+  // total_meth is the sum over all relevant cytosines of the methylation
+  // level estimated at that cytosine using the mapped reads. This value
+  // contributes to the unweighted mean methylation.
   double total_meth{};
 
-  // coverage is equal to total_c plus total_t. These are the counts
-  // that contribute towards estimates of the various methylation
-  // levels.
+  // coverage is equal to total_c plus total_t. These are the counts that
+  // contribute towards estimates of the various methylation levels.
   [[nodiscard]] std::uint64_t
   coverage() const {
     return total_c + total_t;
@@ -92,22 +91,21 @@ struct LevelsCounter {
            std::max(coverage(), static_cast<std::uint64_t>(1));
   }
 
-  // fractional_meth is the fraction of "called" sites that are called
-  // methylated. It is the ratio of called_meth divided by
-  // total_called. This value is always between 0 and 1.
-  [[nodiscard]] double
-  fractional_meth() const {
-    return static_cast<double>(called_meth) /
-           std::max(total_called(), static_cast<std::uint64_t>(1));
-  }
-
-  // mean_meth is the unweighted mean methylation level. This is the
-  // ratio of total_meth divided by sites_covered. This value is
-  // always between 0 and 1.
+  // mean_meth is the unweighted mean methylation level. This is the ratio of
+  // total_meth divided by sites_covered. Value is always between 0 and 1.
   [[nodiscard]] double
   mean_meth() const {
     return static_cast<double>(total_meth) /
            std::max(sites_covered, static_cast<std::uint64_t>(1));
+  }
+
+  // fractional_meth is the fraction of "called" sites that are called
+  // methylated. It is the ratio of called_meth divided by total_called. This
+  // value is always between 0 and 1.
+  [[nodiscard]] double
+  fractional_meth() const {
+    return static_cast<double>(called_meth) /
+           std::max(total_called(), static_cast<std::uint64_t>(1));
   }
 
   explicit LevelsCounter(const std::string &c) : context{c} {}

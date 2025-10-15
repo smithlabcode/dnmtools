f7e96ad0bb9fec9617ceaa0f9a5d9344555327b1
Andrew D Smith <andrewds@usc.edu>
2025-10-15 16:18:11 -0700

    src/radmeth/radmeth.cpp: factoring out code shared by radmeth and radmeth-nano

--- src/radmeth/radmeth.cpp
+++ src/radmeth/radmeth.cpp
@@ -14,8 +14,10 @@
  * more details.
  */
 
+#include "radmeth_design.hpp"
 #include "radmeth_model.hpp"
 #include "radmeth_optimize.hpp"
+#include "radmeth_utils.hpp"
 
 // smithlab headers
 #include "GenomicRegion.hpp"
@@ -26,7 +28,6 @@
 #include <algorithm>
 #include <chrono>
 #include <cmath>
-#include <filesystem>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -36,169 +37,25 @@
 #include <thread>
 #include <vector>
 
-[[nodiscard]] static std::string
-format_duration(const std::chrono::duration<double> elapsed) {
-  static constexpr auto s_per_h = 3600;
-  static constexpr auto s_per_m = 60;
-  const double tot_s = elapsed.count();
-
-  // break down into hours, minutes, seconds
-  const std::uint32_t hours = tot_s / 3600;
-  const std::uint32_t minutes = (static_cast<int>(tot_s) % s_per_h) / s_per_m;
-  const double seconds = tot_s - (hours * s_per_h) - (minutes * s_per_m);
-
-  std::ostringstream oss;
-  oss << std::setfill('0') << std::setw(2) << hours << ":" << std::setfill('0')
-      << std::setw(2) << minutes << ":" << std::fixed << std::setprecision(2)
-      << std::setw(5) << seconds;
-  return oss.str();
-}
-
-struct file_progress {
-  double one_thousand_over_filesize{};
-  std::size_t prev_offset{};
-  explicit file_progress(const std::string &filename) :
-    one_thousand_over_filesize{1000.0 / std::filesystem::file_size(filename)} {}
-  void
-  operator()(std::ifstream &in) {
-    const std::size_t curr_offset =
-      in.eof() ? 1000 : in.tellg() * one_thousand_over_filesize;
-    if (curr_offset <= prev_offset)
-      return;
-    std::ios old_state(nullptr);
-    old_state.copyfmt(std::cerr);
-    std::cerr << "\r[progress: " << std::setw(5) << std::fixed
-              << std::setprecision(1) << (curr_offset / 10.0)
-              << (curr_offset == 1000 ? "%]\n" : "%]");
-    std::cerr.copyfmt(old_state);
-    prev_offset = (curr_offset == 1000)
-                    ? std::numeric_limits<std::size_t>::max()
-                    : curr_offset;
-  }
-};
-
+template <typename RegressionType>
 [[nodiscard]] static bool
-consistent_sample_names(const Regression &reg, const std::string &header) {
-  std::istringstream iss(header);
-  auto nm_itr(std::begin(reg.design.sample_names));
-  const auto nm_end(std::end(reg.design.sample_names));
-  std::string token;
-  while (iss >> token && nm_itr != nm_end)
-    if (token != *nm_itr++)
-      return false;
-  return true;
-}
-
-[[nodiscard]] static std::vector<std::string>
-get_sample_names_from_header(const std::string &header) {
-  std::istringstream iss(header);
-  std::string token;
-  std::vector<std::string> sample_names;
-  while (iss >> token)
-    sample_names.push_back(token);
-  return sample_names;
-}
-
-// Series representation for the lower incomplete gamma P(a,x)
-[[nodiscard]] static double
-gamma_p_series(const double a, const double x) {
-  constexpr auto eps = std::numeric_limits<double>::epsilon();
-  constexpr auto max_iter = 100;
-  double sum = 1.0 / a;
-  double term = sum;
-  for (auto n = 1; n < max_iter; ++n) {
-    term *= x / (a + n);
-    sum += term;
-    if (term < eps * sum)
-      break;
-  }
-  return sum * std::exp(-x + a * std::log(x) - std::lgamma(a));
-}
-
-[[nodiscard]] static inline double
-safe_floor(const double x, const double floor_val) {
-  return std::abs(x) < floor_val ? floor_val : x;
-}
-
-// Continued fraction representation for the upper incomplete gamma Q(a,x)
-[[nodiscard]] static double
-gamma_q_contfrac(const double a, const double x) {
-  constexpr auto epsilon = std::numeric_limits<double>::epsilon();
-  constexpr auto fpmin = std::numeric_limits<double>::min() / epsilon;
-  constexpr auto max_iter = 100;
-  double b = x + 1 - a;
-  double c = 1.0 / fpmin;
-  double d = 1.0 / b;
-  double h = d;
-  for (auto i = 1; i < max_iter; ++i) {
-    const double an = -i * (i - a);
-    b += 2;
-    d = safe_floor(an * d + b, fpmin);
-    c = safe_floor(b + an / c, fpmin);
-    d = 1.0 / d;
-    const double delta = d * c;
-    h *= delta;
-    if (std::abs(delta - 1.0) < epsilon)
-      break;
-  }
-  return std::exp(-x + a * std::log(x) - std::lgamma(a)) * h;
-}
-
-// Regularized lower incomplete gamma P(a,x)
-[[nodiscard]] static double
-gamma_p(const double a, const double x) {
-  if (x < 0 || a <= 0)
-    return 0.0;
-  if (x == 0)
-    return 0.0;
-  if (x < a + 1.0)
-    return gamma_p_series(a, x);
-  return 1.0 - gamma_q_contfrac(a, x);
-}
-
-// chi-square CDF: P(k/2, x/2)
-[[nodiscard]] static double
-chi_square_cdf(const double x, const double k) {
-  return gamma_p(k * 0.5, x * 0.5);
-}
-
-// Given the maximum likelihood estimates of the full and reduced models, the
-// function outputs the p-value of the log-likelihood ratio. *Note* that it is
-// assumed that the reduced model has one fewer factor than the reduced model.
-[[nodiscard]] static double
-llr_test(const double null_loglik, const double full_loglik) {
-  // The log-likelihood ratio statistic.
-  const double log_lik_stat = -2 * (null_loglik - full_loglik);
-
-  // It is assumed that null model has one fewer factor than the full
-  // model. Hence the number of degrees of freedom is 1.
-  const std::size_t degrees_of_freedom = 1;
-
-  // Log-likelihood ratio statistic has a chi-sqare distribution.
-  const double chisq_p = chi_square_cdf(log_lik_stat, degrees_of_freedom);
-
-  const double p_value = 1.0 - chisq_p;
-
-  return p_value;
-}
-
-static bool
-has_low_coverage(const Regression &reg, const std::size_t test_factor) {
+has_low_coverage(const RegressionType &reg, const std::size_t test_factor) {
   bool cvrd_in_test_fact_smpls = false;
   const auto &tcol = reg.design.tmatrix[test_factor];
   for (std::size_t i = 0; i < reg.n_samples() && !cvrd_in_test_fact_smpls; ++i)
-    cvrd_in_test_fact_smpls = (tcol[i] == 1 && reg.props.mc[i].n_reads != 0);
+    cvrd_in_test_fact_smpls = (tcol[i] == 1 && reg.mc[i].n_reads != 0);
 
   bool cvrd_in_other_smpls = false;
   for (std::size_t i = 0; i < reg.n_samples() && !cvrd_in_other_smpls; ++i)
-    cvrd_in_other_smpls = (tcol[i] != 1 && reg.props.mc[i].n_reads != 0);
+    cvrd_in_other_smpls = (tcol[i] != 1 && reg.mc[i].n_reads != 0);
 
   return !cvrd_in_test_fact_smpls || !cvrd_in_other_smpls;
 }
 
+template <typename RegressionType>
 [[nodiscard]] static bool
-has_extreme_counts(const Regression &reg) {
-  const auto &mc = reg.props.mc;
+has_extreme_counts(const RegressionType &reg) {
+  const auto &mc = reg.mc;
 
   bool full_meth = true;
   for (auto i = std::cbegin(mc); i != std::cend(mc) && full_meth; ++i)
@@ -211,42 +68,6 @@ has_extreme_counts(const Regression &reg) {
   return full_meth || no_meth;
 }
 
-[[nodiscard]] static bool
-has_two_values(const Regression &reg, const std::size_t test_factor) {
-  const auto &tcol = reg.design.tmatrix[test_factor];
-  for (const auto x : tcol)
-    if (x != tcol[0])
-      return true;
-  return false;
-}
-
-[[nodiscard]] static std::uint32_t
-get_test_factor_idx(const Regression &model, const std::string &test_factor) {
-  const auto &factors = model.design.factor_names;
-  const auto itr =
-    std::find(std::cbegin(factors), std::cend(factors), test_factor);
-
-  if (itr == std::cend(factors))
-    throw std::runtime_error("factor not part of design: " + test_factor);
-
-  return std::distance(std::cbegin(factors), itr);
-}
-
-[[nodiscard]] static Design
-read_design(const std::string &design_filename) {
-  std::ifstream design_file(design_filename);
-  if (!design_file)
-    throw std::runtime_error("could not open file: " + design_filename);
-  Design design;
-  design_file >> design;
-  return design;
-}
-
-[[nodiscard]] static inline double
-overdispersion_factor(const std::uint32_t n_samples, const double dispersion) {
-  return (n_samples - 1) / (dispersion + 1);
-}
-
 enum class row_status : std::uint8_t {
   ok,
   na,
@@ -254,11 +75,12 @@ enum class row_status : std::uint8_t {
   na_extreme_cnt,
 };
 
+template <typename RegressionType>
 static void
 radmeth(const bool show_progress, const bool more_na_info,
         const std::uint32_t n_threads, const std::string &table_filename,
-        const std::string &outfile, const Regression &alt_model,
-        const Regression &null_model, const std::uint32_t test_factor_idx) {
+        const std::string &outfile, const RegressionType &alt_model,
+        const RegressionType &null_model, const std::uint32_t test_factor_idx) {
   static constexpr auto buf_size = 1024;
   static constexpr auto max_lines = 16384;
 
@@ -274,7 +96,7 @@ radmeth(const bool show_progress, const bool more_na_info,
   std::getline(table_file, sample_names_header);
 
   const auto sample_names = get_sample_names_from_header(sample_names_header);
-  if (!consistent_sample_names(alt_model, sample_names_header)) {
+  if (!consistent_sample_names(alt_model.design, sample_names_header)) {
     // clang-format off
     const auto message =
       R"(
@@ -298,8 +120,8 @@ that the design matrix and the proportion table are correctly formatted.
   std::vector<int> n_bytes(max_lines, 0);
   std::vector<std::string> lines(max_lines);
 
-  std::vector<Regression> alt_models(n_threads, alt_model);
-  std::vector<Regression> null_models(n_threads, null_model);
+  std::vector<RegressionType> alt_models(n_threads, alt_model);
+  std::vector<RegressionType> null_models(n_threads, null_model);
 
   const auto n_groups = alt_model.n_groups();
 
@@ -328,7 +150,7 @@ that the design matrix and the proportion table are correctly formatted.
           // balances work better.
           if (b % n_threads != thread_id)
             continue;
-          t_alt_model.props.parse(lines[b]);
+          t_alt_model.parse(lines[b]);
           if (t_alt_model.props_size() != n_samples)
             throw std::runtime_error("found row with wrong number of columns");
 
@@ -344,7 +166,8 @@ that the design matrix and the proportion table are correctly formatted.
 
             fit_regression_model(t_alt_model, p_estim_alt, phi_estim_alt);
 
-            t_null_model.props = t_alt_model.props;
+            t_null_model.mc = t_alt_model.mc;
+            t_null_model.rowname = t_alt_model.rowname;
 
             fit_regression_model(t_null_model, p_estim_null, phi_estim_null);
 
@@ -361,7 +184,7 @@ that the design matrix and the proportion table are correctly formatted.
           n_bytes[b] = [&] {
             // clang-format off
             const int n_prefix_bytes =
-              std::sprintf(bufs[b].data(), "%s\t", t_alt_model.rowname().data());
+              std::sprintf(bufs[b].data(), "%s\t", t_alt_model.rowname.data());
             // clang-format on
             if (n_prefix_bytes < 0)
               return n_prefix_bytes;
@@ -432,19 +255,6 @@ that the design matrix and the proportion table are correctly formatted.
     progress(table_file);
 }
 
-static void
-ensure_sample_order(const std::string &table_filename, Regression &alt_model,
-                    Regression &null_model) {
-  std::ifstream table_file(table_filename);
-  if (!table_file)
-    throw std::runtime_error("could not open file: " + table_filename);
-  std::string header;
-  std::getline(table_file, header);
-  const auto sample_names = get_sample_names_from_header(header);
-  alt_model.order_samples(sample_names);
-  null_model.order_samples(sample_names);
-}
-
 int
 main_radmeth(int argc, char *argv[]) {
   try {
@@ -480,7 +290,6 @@ main_radmeth(int argc, char *argv[]) {
     opt_parse.add_opt("max-iter", '\0',
                       "max iterations when estimating parameters", false,
                       Regression::max_iter);
-
     std::vector<std::string> leftover_args;
     opt_parse.parse(argc, argv, leftover_args);
     if (argc == 1 || opt_parse.help_requested()) {
@@ -507,30 +316,26 @@ main_radmeth(int argc, char *argv[]) {
     if (verbose)
       std::cerr << "design table filename: " << design_filename << "\n\n";
 
-    // initialize full design matrix from file
-    Regression alt_model;
-    alt_model.design = read_design(design_filename);
-
+    Design design = Design::read_design(design_filename);
     if (verbose)
-      std::cerr << "Alternate model:\n" << alt_model.design << '\n';
+      std::cerr << "Alternate model:\n" << design << '\n';
 
     // Check that provided test factor name exists and find its index.
-    const auto test_factor_idx = get_test_factor_idx(alt_model, test_factor);
+    const auto test_factor_idx = design.get_test_factor_idx(test_factor);
+
+    ensure_sample_order(table_filename, design);
 
     // verify that the design includes more than one level for the
     // test factor
-    if (!has_two_values(alt_model, test_factor_idx)) {
-      const auto first_level = alt_model.design.matrix[0][test_factor_idx];
+    if (!design.has_two_values(test_factor_idx)) {
+      const auto first_level = design.matrix[0][test_factor_idx];
       throw std::runtime_error("test factor only one level: " + test_factor +
                                ", " + std::to_string(first_level));
     }
 
-    Regression null_model = alt_model;
-    null_model.design = alt_model.design.drop_factor(test_factor_idx);
+    const Design null_design = design.drop_factor(test_factor_idx);
     if (verbose)
-      std::cerr << "Null model:\n" << null_model.design << '\n';
-
-    ensure_sample_order(table_filename, alt_model, null_model);
+      std::cerr << "Null model:\n" << null_design << '\n';
 
     // clang-format off
     if (verbose)
@@ -546,6 +351,12 @@ main_radmeth(int argc, char *argv[]) {
                 << "estimated methylation is for test factor value (0 or 1)\n"
                 << '\n';
     // clang-format on
+
+    Regression alt_model;
+    alt_model.design = design;
+    Regression null_model;
+    null_model.design = null_design;
+
     const auto start_time = std::chrono::steady_clock::now();
     radmeth(show_progress, more_na_info, n_threads, table_filename, outfile,
             alt_model, null_model, test_factor_idx);

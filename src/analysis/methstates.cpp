c2fcf3a37afd1194839cc95eb50c2b94d66f865d
Andrew D Smith <andrewds@usc.edu>
2025-11-24 17:02:31 -0800

    src/analysis/methstates.cpp: changes to add static analysis

--- src/analysis/methstates.cpp
+++ src/analysis/methstates.cpp
@@ -17,26 +17,30 @@
  * General Public License for more details.
  */
 
-#include <algorithm>
-#include <iostream>
-#include <numeric>
-#include <stdexcept>
-#include <string>
-#include <unordered_set>
-#include <vector>
-
 #include "OptionParser.hpp"
 #include "bam_record_utils.hpp"
-#include "cigar_utils.hpp"
 #include "dnmt_error.hpp"
-#include "htslib_wrapper.hpp"
-#include "sam_record.hpp"
 #include "smithlab_os.hpp"
-#include "smithlab_utils.hpp"
+
+#include <bamxx.hpp>
+
+#include <algorithm>
+#include <cctype>
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
 using std::cerr;
 using std::cout;
-using std::endl;
 using std::lower_bound;
 using std::string;
 using std::unordered_map;
@@ -45,26 +49,31 @@ using std::vector;
 
 using bamxx::bam_rec;
 
-static const char b2c[] = "TNGNNNCNNNNNNNNNNNNA";
+static const char b2c[] = "TNGNNNCNNNNNNNNNNNNA";  // NOLINT(*-avoid-c-arrays)
 
 struct quick_buf : public std::ostringstream,
                    public std::basic_stringbuf<char> {
   // ADS: consider putting this class in a header somewhere
   quick_buf() { static_cast<std::basic_ios<char> &>(*this).rdbuf(this); }
 
-  void clear() { setp(pbase(), pbase()); }
+  void
+  clear() {
+    setp(pbase(), pbase());
+  }
 
-  char const *c_str() {
+  char const *
+  c_str() {
     *pptr() = '\0';
     return pbase();
   }
 };
 
-template<class BidirIt, class OutputIt>
+template <class BidirIt, class OutputIt>
 // constexpr // since C++20
 OutputIt
 revcomp_copy(BidirIt first, BidirIt last, OutputIt d_first) {
-  for (; first != last; ++d_first) *d_first = b2c[*(--last) - 'A'];
+  for (; first != last; ++d_first)
+    *d_first = b2c[*(--last) - 'A'];  // NOLINT(*-constant-array-index)
   return d_first;
 }
 
@@ -78,7 +87,8 @@ collect_cpgs(const string &s, vector<uint64_t> &cpgs) {
   cpgs.clear();
   const uint64_t lim = std::size(s) - 1;
   for (auto i = 0u; i < lim; ++i)
-    if (is_cpg(s, i)) cpgs.push_back(i);
+    if (is_cpg(s, i))
+      cpgs.push_back(i);
 }
 
 static bool
@@ -102,12 +112,14 @@ convert_meth_states_pos(const vector<uint64_t> &cpgs,
   auto cpg_itr = lower_bound(begin(cpgs), end(cpgs), seq_start);
   auto first_cpg_itr = end(cpgs);
 
-  if (cpg_itr == end(cpgs)) return false;
+  if (cpg_itr == end(cpgs))
+    return false;
 
   for (; cpg_itr != end(cpgs) && *cpg_itr < seq_end; cpg_itr++) {
     const char x = seq_str[*cpg_itr - seq_start];
     states += (x == 'T') ? 'T' : ((x == 'C') ? 'C' : 'N');
-    if (first_cpg_itr == end(cpgs)) first_cpg_itr = cpg_itr;
+    if (first_cpg_itr == end(cpgs))
+      first_cpg_itr = cpg_itr;
   }
 
   if (first_cpg_itr != end(cpgs))
@@ -152,12 +164,15 @@ convert_meth_states_neg(const vector<uint64_t> &cpgs,
     lower_bound(begin(cpgs), end(cpgs), seq_start > 0 ? seq_start - 1 : 0);
   auto first_cpg_itr = end(cpgs);
 
-  if (cpg_itr == end(cpgs)) { return false; }
+  if (cpg_itr == end(cpgs)) {
+    return false;
+  }
   else {
     for (; cpg_itr != end(cpgs) && *cpg_itr < seq_end - 1; cpg_itr++) {
       const char x = seq_str[*cpg_itr - seq_start + 1];
       states += (x == 'G') ? 'C' : ((x == 'A') ? 'T' : 'N');
-      if (first_cpg_itr == end(cpgs)) first_cpg_itr = cpg_itr;
+      if (first_cpg_itr == end(cpgs))
+        first_cpg_itr = cpg_itr;
     }
   }
 
@@ -176,20 +191,21 @@ get_chrom(const string &chrom_name, const vector<string> &all_chroms,
     throw dnmt_error("could not find chrom: " + chrom_name);
 
   chrom = all_chroms[the_chrom->second];
-  if (chrom.empty()) throw dnmt_error("problem with chrom: " + chrom_name);
+  if (chrom.empty())
+    throw dnmt_error("problem with chrom: " + chrom_name);
 }
 
 int
-main_methstates(int argc, char *argv[]) {
+main_methstates(int argc, char *argv[]) {  // NOLINT(*-avoid-c-arrays)
   try {
-    const string description =
-      "Convert mapped reads in SAM format into a format that indicates binary \
-      sequences of methylation states in each read, indexed by the identity   \
-      of the CpG they cover, along with the chromosome. Only reads that       \
-      cover a CpG site are included in the output. All output is relative to  \
-      the positive reference strand. This format is used as input to other    \
-      tools, and is not intended to be human-interpretable. All chromosome    \
-      sequences are loaded at once.";
+    const auto description =
+      R"(Convert mapped reads in SAM format into a format that indicates binary
+      sequences of methylation states in each read, indexed by the identity
+      of the CpG they cover, along with the chromosome. Only reads that
+      cover a CpG site are included in the output. All output is relative to
+      the positive reference strand. This format is used as input to other
+      tools, and is not intended to be human-interpretable. All chromosome
+      sequences are loaded at once.)";
 
     bool VERBOSE = false;
     bool compress_output = false;
@@ -199,7 +215,8 @@ main_methstates(int argc, char *argv[]) {
     int n_threads = 1;
 
     /****************** COMMAND LINE OPTIONS ********************/
-    OptionParser opt_parse(argv[0], description, "<sam-file>");
+    OptionParser opt_parse(argv[0],  // NOLINT(*-pointer-arithmetic)
+                           description, "<sam-file>");
     opt_parse.add_opt("output", 'o', "output file name", false, outfile);
     opt_parse.add_opt("chrom", 'c', "fasta format reference genome file", true,
                       chrom_file);
@@ -211,26 +228,27 @@ main_methstates(int argc, char *argv[]) {
     vector<string> leftover_args;
     opt_parse.parse(argc, argv, leftover_args);
     if (argc == 1 || opt_parse.help_requested()) {
-      cerr << opt_parse.help_message() << endl
-           << opt_parse.about_message() << endl;
+      cerr << opt_parse.help_message() << '\n'
+           << opt_parse.about_message() << '\n';
       return EXIT_SUCCESS;
     }
     if (opt_parse.about_requested()) {
-      cerr << opt_parse.about_message() << endl;
+      cerr << opt_parse.about_message() << '\n';
       return EXIT_SUCCESS;
     }
     if (opt_parse.option_missing()) {
-      cerr << opt_parse.option_missing_message() << endl;
+      cerr << opt_parse.option_missing_message() << '\n';
       return EXIT_SUCCESS;
     }
     if (leftover_args.size() != 1) {
-      cerr << opt_parse.help_message() << endl;
+      cerr << opt_parse.help_message() << '\n';
       return EXIT_SUCCESS;
     }
     const string mapped_reads_file = leftover_args.front();
     /****************** END COMMAND LINE OPTIONS *****************/
 
-    if (n_threads < 0) throw dnmt_error("thread count cannot be negative");
+    if (n_threads < 0)
+      throw dnmt_error("thread count cannot be negative");
 
     /* first load in all the chromosome sequences and names, and make
        a map from chromosome name to the location of the chromosome
@@ -245,19 +263,23 @@ main_methstates(int argc, char *argv[]) {
     for (uint64_t i = 0; i < chrom_names.size(); ++i)
       chrom_lookup[chrom_names[i]] = i;
 
-    if (VERBOSE) cerr << "n_chroms: " << all_chroms.size() << endl;
+    if (VERBOSE)
+      cerr << "n_chroms: " << all_chroms.size() << '\n';
 
     bamxx::bam_tpool tp(n_threads);  // declared first; destroyed last
 
     bamxx::bam_in in(mapped_reads_file);
-    if (!in) throw dnmt_error("cannot open input file " + mapped_reads_file);
+    if (!in)
+      throw dnmt_error("cannot open input file " + mapped_reads_file);
     bamxx::bam_header hdr(in);
-    if (!hdr) throw dnmt_error("cannot read heade" + mapped_reads_file);
+    if (!hdr)
+      throw dnmt_error("cannot read heade" + mapped_reads_file);
 
     // open the output file
     const string output_mode = compress_output ? "w" : "wu";
     bamxx::bgzf_file out(outfile, output_mode);
-    if (!out) throw dnmt_error("error opening output file: " + outfile);
+    if (!out)
+      throw dnmt_error("error opening output file: " + outfile);
 
     /* set the threads for the input file decompression */
     if (n_threads > 1) {
@@ -285,7 +307,8 @@ main_methstates(int argc, char *argv[]) {
         if (chroms_seen.find(chrom_name) != end(chroms_seen))
           throw dnmt_error("chroms out of order (check SAM file sorted)");
 
-        if (VERBOSE) cerr << "processing " << chrom_name << endl;
+        if (VERBOSE)
+          cerr << "processing " << chrom_name << '\n';
 
         get_chrom(chrom_name, all_chroms, chrom_lookup, chrom);
         collect_cpgs(chrom, cpgs);
@@ -304,14 +327,14 @@ main_methstates(int argc, char *argv[]) {
         buf << sam_hdr_tid2name(hdr, aln) << '\t' << first_cpg_index << '\t'
             << seq << '\n';
         if (!out.write(buf.c_str(), buf.tellp())) {
-          cerr << "failure writing output" << endl;
+          cerr << "failure writing output" << '\n';
           return EXIT_FAILURE;
         }
       }
     }
   }
   catch (const std::exception &e) {
-    cerr << e.what() << endl;
+    cerr << e.what() << '\n';
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;

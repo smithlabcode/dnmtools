cc0795b68fe012a6adab1d7e760fba70cd0e63d8
Andrew D Smith <andrewds@usc.edu>
2025-11-24 17:02:32 -0800

    src/common/Epiread.hpp: changes to add static analysis

--- src/common/Epiread.hpp
+++ src/common/Epiread.hpp
@@ -17,29 +17,39 @@
 #ifndef EPIREAD
 #define EPIREAD
 
+#include <cstddef>
+#include <iosfwd>
 #include <string>
 #include <vector>
-#include "smithlab_utils.hpp"
 
 struct epiread {
   std::string chr{};
   size_t pos{};
   std::string seq{};
   epiread() = default;
-  epiread(const std::string &line);
+  explicit epiread(const std::string &line);
   epiread(const size_t p, const std::string &s) : pos(p), seq(s) {}
-  epiread(const std::string &c, const size_t p, const std::string &s)
-      : chr(c), pos(p), seq(s) {}
+  epiread(const std::string &c, const size_t p, const std::string &s) :
+    chr(c), pos(p), seq(s) {}
 
-  bool operator<(const epiread &other) const {
+  bool
+  operator<(const epiread &other) const {
     return (chr < other.chr || (chr == other.chr && pos < other.pos));
   }
-  size_t end() const {return pos + seq.length();}
-  size_t length() const {return seq.length();}
+  size_t
+  end() const {
+    return pos + seq.length();
+  }
+  size_t
+  length() const {
+    return seq.length();
+  }
 };
 
-std::istream& operator>>(std::istream &in, epiread &er);
-std::ostream& operator<<(std::ostream &out, const epiread &er);
+std::istream &
+operator>>(std::istream &in, epiread &er);
+std::ostream &
+operator<<(std::ostream &out, const epiread &er);
 
 size_t
 adjust_read_offsets(std::vector<epiread> &reads);

50b79d03a47e99d5433044c8a7905b8b10c0d67f
Andrew D Smith <andrewds@usc.edu>
2025-11-24 17:02:31 -0800

    src/analysis/metagene.cpp: changes to add static analysis

--- src/analysis/metagene.cpp
+++ src/analysis/metagene.cpp
@@ -19,20 +19,29 @@
  * <http://www.gnu.org/licenses/>.
  */
 
-#include <fstream>
-#include <stdexcept>
-#include <unordered_map>
-
 #include "GenomicRegion.hpp"
 #include "LevelsCounter.hpp"
 #include "MSite.hpp"
 #include "OptionParser.hpp"
-#include "bsutils.hpp"
-#include "smithlab_utils.hpp"
+
+#include "bamxx.hpp"
+
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 using std::cerr;
 using std::cout;
-using std::endl;
 using std::ostream;
 using std::pair;
 using std::runtime_error;
@@ -77,37 +86,40 @@ process_chrom(const uint32_t region_size, const vector<GenomicRegion> &genes,
   }
 }
 
-template<typename T> static void
+template <typename T>
+static void
 collapse_bins(const uint32_t bin_size, vector<T> &v) {
   const uint32_t n_bins = std::ceil(static_cast<double>(v.size()) / bin_size);
   vector<T> vv(n_bins);
-  for (auto i = 0u; i < v.size(); ++i) vv[i / bin_size] += v[i];
+  for (auto i = 0u; i < v.size(); ++i)
+    vv[i / bin_size] += v[i];
   v.swap(vv);
 }
 
 int
-metagene(int argc, char *argv[]) {
+metagene(int argc, char *argv[]) {  // NOLINT(*-avoid-c-arrays)
   constexpr auto description =
-    "Compute the information needed for metagene plots of DNA methylation    \
-     levels. The columns in the output correspond to the fields calculated   \
-     globally by the `levels` and per-region by the `roi` command. Input     \
-     for features is in BED format, and when present the 6th column is used  \
-     to indicate strand. For features of non-zero width (where the 2nd and   \
-     3rd columns are not identical) the negative strand will indicate that   \
-     3rd column should be used. This means, for example, if the features are \
-     genes, and the promoters are of interest, the strand will be used       \
-     correctly.";
-
+    R"(
+Compute the information needed for metagene plots of DNA methylation
+levels. The columns in the output correspond to the fields calculated
+globally by the `levels` and per-region by the `roi` command. Input
+for features is in BED format, and when present the 6th column is used
+to indicate strand. For features of non-zero width (where the 2nd and
+3rd columns are not identical) the negative strand will indicate that
+3rd column should be used. This means, for example, if the features are
+genes, and the promoters are of interest, the strand will be used
+correctly.
+)";
   try {
     string outfile;
-    uint32_t region_size = 5000;
+    uint32_t region_size = 5000;  // NOLINT(*-avoid-magic-numbers)
     bool verbose = false;
     bool show_progress = false;
-    uint32_t bin_size = 50;
+    uint32_t bin_size = 50;  // NOLINT(*-avoid-magic-numbers)
 
     /****************** GET COMMAND LINE ARGUMENTS ***************************/
-    OptionParser opt_parse(strip_path(argv[0]), description,
-                           "<features-bed> <counts>");
+    OptionParser opt_parse(argv[0],  // NOLINT(*-pointer-arithmetic)
+                           description, "<features-bed> <counts>");
     opt_parse.add_opt("output", 'o', "output file (default: terminal)", false,
                       outfile);
     opt_parse.add_opt("size", 's', "analyze this size in both directions",
@@ -118,31 +130,32 @@ metagene(int argc, char *argv[]) {
     vector<string> leftover_args;
     opt_parse.parse(argc, argv, leftover_args);
     if (argc == 1 || opt_parse.help_requested()) {
-      cerr << opt_parse.help_message() << endl;
+      cerr << opt_parse.help_message() << '\n';
       return EXIT_SUCCESS;
     }
     if (opt_parse.about_requested()) {
-      cerr << opt_parse.about_message() << endl;
+      cerr << opt_parse.about_message() << '\n';
       return EXIT_SUCCESS;
     }
     if (opt_parse.option_missing()) {
-      cerr << opt_parse.option_missing_message() << endl;
+      cerr << opt_parse.option_missing_message() << '\n';
       return EXIT_SUCCESS;
     }
     if (leftover_args.size() != 2) {
-      cerr << opt_parse.help_message() << endl;
+      cerr << opt_parse.help_message() << '\n';
       return EXIT_SUCCESS;
     }
     const string features_file_name = leftover_args.front();
     const string cpg_file_name = leftover_args.back();
     /**********************************************************************/
 
-    if (verbose) cerr << "[loading feature annotations data]" << endl;
+    if (verbose)
+      cerr << "[loading feature annotations data]" << '\n';
     vector<GenomicRegion> features;
     ReadBEDFile(features_file_name, features);
     sort(begin(features), end(features));
     if (verbose)
-      cerr << "[number of features: " << features.size() << "]" << endl;
+      cerr << "[number of features: " << features.size() << "]" << '\n';
 
     // identify the start and end of ranges for each chromosome
     unordered_map<string, pair<uint32_t, uint32_t>> lookup;
@@ -152,7 +165,8 @@ metagene(int argc, char *argv[]) {
     auto prev_idx = 0u;
     for (auto i = 0u; i < features.size(); ++i)
       if (features[i].get_chrom() != chrom_name) {
-        if (!chrom_name.empty()) lookup.insert({chrom_name, {prev_idx, i}});
+        if (!chrom_name.empty())
+          lookup.insert({chrom_name, {prev_idx, i}});
         prev_idx = i;
         chrom_name = features[i].get_chrom();
       }
@@ -167,7 +181,8 @@ metagene(int argc, char *argv[]) {
     vector<LevelsCounter> levels(2 * region_size);
 
     bamxx::bgzf_file cpgin(cpg_file_name, "r");
-    if (!cpgin) throw runtime_error("failed to open file: " + cpg_file_name);
+    if (!cpgin)
+      throw runtime_error("failed to open file: " + cpg_file_name);
 
     vector<MSite> sites;
     string line;
@@ -182,10 +197,11 @@ metagene(int argc, char *argv[]) {
           const auto n_features = pair_diff(bounds);
           if (show_progress)
             cerr << "[sites=" << sites.size() << " features=" << n_features
-                 << "]" << endl;
+                 << "]" << '\n';
           sites.clear();
         }
-        if (show_progress) cerr << "[processing: " << the_site.chrom << "]";
+        if (show_progress)
+          cerr << "[processing: " << the_site.chrom << "]";
         chrom_name = the_site.chrom;
       }
       sites.push_back(std::move(the_site));
@@ -198,24 +214,24 @@ metagene(int argc, char *argv[]) {
       const auto n_features = pair_diff(bounds);
       if (show_progress)
         cerr << "[sites=" << sites.size() << " features=" << n_features << "]"
-             << endl;
+             << '\n';
     }
 
     collapse_bins(bin_size, levels);
 
     if (verbose)
-      cerr << "output columns:\n"
-           << LevelsCounter::format_header() << endl;
+      cerr << "output columns:\n" << LevelsCounter::format_header() << '\n';
 
     std::ofstream of;
-    if (!outfile.empty()) of.open(outfile);
+    if (!outfile.empty())
+      of.open(outfile);
     std::ostream out(of.is_open() ? of.rdbuf() : std::cout.rdbuf());
 
     for (auto i = 0u; i < levels.size(); ++i)
-      out << i * bin_size << '\t' << levels[i].format_row() << endl;
+      out << i * bin_size << '\t' << levels[i].format_row() << '\n';
   }
   catch (std::exception &e) {
-    cerr << e.what() << endl;
+    cerr << e.what() << '\n';
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;

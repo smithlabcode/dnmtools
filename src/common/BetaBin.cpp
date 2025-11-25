9c61cbaf99636e70ed1df57c81b2db03d396dea6
Andrew D Smith <andrewds@usc.edu>
2025-11-24 17:02:32 -0800

    src/common/BetaBin.cpp: changes to add static analysis

--- src/common/BetaBin.cpp
+++ src/common/BetaBin.cpp
@@ -17,26 +17,26 @@
 
 #include "BetaBin.hpp"
 
+#include <algorithm>
 #include <cmath>
-
+#include <cstdint>
 #include <iomanip>
-#include <numeric>
 #include <iostream>
+#include <numeric>
 #include <sstream>
+#include <utility>
 
-#include <gsl/gsl_sf_psi.h>
 #include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_sf_psi.h>
 
-
-using std::vector;
-using std::pair;
-using std::setw;
+using std::cerr;
 using std::max;
 using std::min;
-using std::cerr;
-using std::endl;
-using std::string;
+using std::pair;
 using std::setprecision;
+using std::setw;
+using std::string;
+using std::vector;
 
 //////////////////////////////////////////////
 //////       struct betabin             //////
@@ -44,8 +44,7 @@ using std::setprecision;
 
 const double betabin::tolerance = 1e-10;
 
-betabin::betabin() :
-  alpha(1), beta(1), lnbeta_helper(gsl_sf_lnbeta(1, 1)) {}
+betabin::betabin() : alpha(1), beta(1), lnbeta_helper(gsl_sf_lnbeta(1, 1)) {}
 
 betabin::betabin(const double a, const double b) :
   alpha(a), beta(b), lnbeta_helper(gsl_sf_lnbeta(a, b)) {}
@@ -56,37 +55,35 @@ betabin::betabin(const string &str) {
   iss >> name >> alpha >> beta;
   if (name != "betabin" || alpha < 0 || beta < 0) {
     cerr << "betabin::betabin: "
-         << "bad string representation of betabin distribution: "
-         << str << endl;
+         << "bad string representation of betabin distribution: " << str
+         << '\n';
     throw "bad string representation of betabin distribution";
   }
   lnbeta_helper = gsl_sf_lnbeta(alpha, beta);
 }
 
-
 string
 betabin::tostring() const {
   std::ostringstream os;
-  os << "betabin " << setprecision(4) << alpha << " "
-     << setprecision(4) << beta;
+  os << "betabin " << setprecision(4) << alpha << " " << setprecision(4)
+     << beta;
   return os.str();
 }
 
-
 double
 betabin::operator()(const pair<double, double> &val) const {
-  const size_t x = static_cast<size_t>(val.first);
-  const size_t n = static_cast<size_t>(x + val.second);
-  return gsl_sf_lnchoose(n, x) +
-    gsl_sf_lnbeta(alpha + x, beta + val.second) - lnbeta_helper;
+  const std::uint32_t x = static_cast<std::uint32_t>(val.first);
+  const std::uint32_t n = static_cast<std::uint32_t>(val.first + val.second);
+  return gsl_sf_lnchoose(n, x) + gsl_sf_lnbeta(alpha + x, beta + val.second) -
+         lnbeta_helper;
 }
 
 double
 betabin::log_likelihood(const pair<double, double> &val) const {
-  const size_t x = static_cast<size_t>(val.first);
-  const size_t n = static_cast<size_t>(x + val.second);
-  return gsl_sf_lnchoose(n, x) +
-    gsl_sf_lnbeta(alpha + x, beta + val.second) - lnbeta_helper;
+  const std::uint32_t x = static_cast<std::uint32_t>(val.first);
+  const std::uint32_t n = static_cast<std::uint32_t>(val.first + val.second);
+  return gsl_sf_lnchoose(n, x) + gsl_sf_lnbeta(alpha + x, beta + val.second) -
+         lnbeta_helper;
 }
 
 double
@@ -96,29 +93,36 @@ betabin::sign(const double x) {
 
 double
 betabin::invpsi(const double tolerance, const double x) {
-  double L = 1.0, Y = std::exp(x);
+  double L = 1.0;
+  double Y = std::exp(x);
   while (L > tolerance) {
-    Y += L*sign(x - gsl_sf_psi(Y));
-    L /= 2.0;
+    Y += L * sign(x - gsl_sf_psi(Y));
+    L /= 2.0;  // NOLINT(*-avoid-magic-numbers)
   }
   return Y;
 }
 
 double
 betabin::movement(const double curr, const double prev) {
-  return std::abs(curr - prev)/std::max(std::fabs(curr), std::fabs(prev));
+  return std::abs(curr - prev) / std::max(std::fabs(curr), std::fabs(prev));
 }
 
 void
 betabin::fit(const vector<double> &vals_a, const vector<double> &vals_b,
              const vector<double> &p) {
+  static constexpr auto initial_param_vals = 0.01;
   const double p_total = std::accumulate(p.begin(), p.end(), 0.0);
-  const double alpha_rhs = inner_product(vals_a.begin(), vals_a.end(),
-                                         p.begin(), 0.0)/p_total;
-  const double beta_rhs = inner_product(vals_b.begin(), vals_b.end(),
-                                        p.begin(), 0.0)/p_total;
+  const double alpha_rhs =
+    std::inner_product(std::cbegin(vals_a), std::cend(vals_a), std::cbegin(p),
+                       0.0) /
+    p_total;
+  const double beta_rhs =
+    std::inner_product(std::cbegin(vals_b), std::cend(vals_b), std::cbegin(p),
+                       0.0) /
+    p_total;
   double prev_alpha = 0.0, prev_beta = 0.0;
-  alpha = beta = 0.01;
+  alpha = beta = initial_param_vals;
+
   while (movement(alpha, prev_alpha) > tolerance &&
          movement(beta, prev_beta) > tolerance) {
     prev_alpha = alpha;

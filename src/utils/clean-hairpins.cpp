aa6e06ce44c069bf36d08cf7e2c923fd72381cf4
Andrew D Smith <andrewds@usc.edu>
2025-11-24 17:02:32 -0800

    src/utils/clean-hairpins.cpp: changes to add static analysis

--- src/utils/clean-hairpins.cpp
+++ src/utils/clean-hairpins.cpp
@@ -16,85 +16,77 @@
  * General Public License for more details.
  */
 
+#include "OptionParser.hpp"
+#include "smithlab_utils.hpp"
+
 #include <bamxx.hpp>
 
 #include <algorithm>
 #include <array>
-#include <cstddef>  // std::size_t
-#include <cstdint>  // std::uint32_t and std::uint64_t
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
 #include <fstream>
-#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <limits>
+#include <numeric>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <vector>
 
-#include "OptionParser.hpp"
-#include "smithlab_os.hpp"
-#include "smithlab_utils.hpp"
-
-using std::array;
-using std::cerr;
-using std::cout;
-using std::endl;
-using std::floor;
-using std::ifstream;
-using std::istream;
-using std::min;
-using std::ofstream;
-using std::ostream;
-using std::ostringstream;
-using std::runtime_error;
-using std::size_t;
-using std::string;
-using std::uint32_t;
-using std::uint64_t;
-using std::vector;
-
-using bamxx::bgzf_file;
+// NOLINTBEGIN(*-narrowing-conversions,*-magic-numbers)
 
 // store each read from one end
 struct FASTQRecord {
-  string name;
-  string seq;
-  string qual;
+  std::string name;
+  std::string seq;
+  std::string qual;
 };
 
-bgzf_file &
-operator<<(bgzf_file &out, const FASTQRecord &r) {
+bamxx::bgzf_file &
+operator<<(bamxx::bgzf_file &out, const FASTQRecord &r) {
   // below, the other chars are @, + and four newlines
-  static const uint32_t other_chars = 6;
-  const size_t buf_size =
+  static const std::uint32_t other_chars = 6;
+  const std::size_t buf_size =
     size(r.name) + size(r.seq) + size(r.qual) + other_chars;
-  string buffer(buf_size, '\0');
-  string::iterator b = begin(buffer);
+  std::string buffer(buf_size, '\0');
+  std::string::iterator b = begin(buffer);
   *b++ = '@';
-  b = copy(cbegin(r.name), cend(r.name), b);
+  b = copy(std::cbegin(r.name), std::cend(r.name), b);
   *b++ = '\n';
-  b = copy(cbegin(r.seq), cend(r.seq), b);
+  b = copy(std::cbegin(r.seq), std::cend(r.seq), b);
   *b++ = '\n';
   *b++ = '+';
   *b++ = '\n';
-  b = copy(cbegin(r.qual), cend(r.qual), b);
+  b = copy(std::cbegin(r.qual), std::cend(r.qual), b);
   *b++ = '\n';
   out.write(buffer);
   return out;
 }
 
 // Read 4 lines one time from fastq and fill in the FASTQRecord structure
-static bgzf_file &
-operator>>(bgzf_file &s, FASTQRecord &r) {
-  constexpr auto n_error_codes = 5u;
+static bamxx::bgzf_file &
+operator>>(bamxx::bgzf_file &s, FASTQRecord &r) {
+  static constexpr auto n_error_codes = 5u;
 
-  enum err_code { none, bad_name, bad_seq, bad_plus, bad_qual };
+  enum err_code : std::uint8_t {
+    none,
+    bad_name,
+    bad_seq,
+    bad_plus,
+    bad_qual,
+  };
 
-  static const array<runtime_error, n_error_codes> error_msg = {
-    runtime_error(""), runtime_error("failed to parse fastq name line"),
-    runtime_error("failed to parse fastq sequence line"),
-    runtime_error("failed to parse fastq plus line"),
-    runtime_error("failed to parse fastq qual line")};
+  static const std::array<std::runtime_error, n_error_codes> error_msg = {
+    std::runtime_error(""),
+    std::runtime_error("failed to parse fastq name line"),
+    std::runtime_error("failed to parse fastq sequence line"),
+    std::runtime_error("failed to parse fastq plus line"),
+    std::runtime_error("failed to parse fastq qual line"),
+  };
 
   err_code ec = err_code::none;
 
@@ -105,13 +97,14 @@ operator>>(bgzf_file &s, FASTQRecord &r) {
     ec = err_code::bad_name;
 
   const auto nm_end = r.name.find_first_of(" \t");
-  const auto nm_sz = (nm_end == string::npos ? r.name.size() : nm_end) - 1;
-  r.name.erase(copy_n(cbegin(r.name) + 1, nm_sz, begin(r.name)), cend(r.name));
+  const auto nm_sz = (nm_end == std::string::npos ? r.name.size() : nm_end) - 1;
+  r.name.erase(copy_n(std::cbegin(r.name) + 1, nm_sz, begin(r.name)),
+               std::cend(r.name));
 
   if (!getline(s, r.seq))
     ec = err_code::bad_seq;
 
-  string tmp;
+  std::string tmp;
   if (!getline(s, tmp))
     ec = err_code::bad_plus;
 
@@ -119,7 +112,7 @@ operator>>(bgzf_file &s, FASTQRecord &r) {
     ec = err_code::bad_qual;
 
   if (ec != err_code::none)
-    throw error_msg[ec];
+    throw error_msg[ec];  // NOLINT
 
   return s;
 }
@@ -132,12 +125,13 @@ similar_letters_bisulfite_tc_and_ag(const char a, const char b) {
 
 // compare two reads to detect the overlapped region
 static double
-similarity_both_bisulfite_conversions(const string &s1, const string &s2) {
-  const size_t lim = min(size(s1), size(s2));
+similarity_both_bisulfite_conversions(const std::string &s1,
+                                      const std::string &s2) {
+  const std::size_t lim = std::min(size(s1), size(s2));
 
-  uint32_t total_letters = 0;
-  uint32_t matching_letters = 0;
-  for (size_t i = 0; i < lim; ++i) {
+  std::uint32_t total_letters = 0;
+  std::uint32_t matching_letters = 0;
+  for (std::size_t i = 0; i < lim; ++i) {
     matching_letters += (similar_letters_bisulfite_tc_and_ag(s1[i], s2[i]));
     total_letters += (valid_base(s1[i]) && valid_base(s2[i]));
   }
@@ -146,20 +140,19 @@ similarity_both_bisulfite_conversions(const string &s1, const string &s2) {
 }
 
 struct hp_summary {
-
   explicit hp_summary(const double cutoff) : cutoff{cutoff} {}
 
   // n_reads is the total number of read pairs in the input fastq
   // files.
-  uint64_t n_reads{};
+  std::uint64_t n_reads{};
 
   // n_good_reads is the total number of read pairs that together have
   // a fraction of good bases above the minimum specified.
-  uint64_t n_good_reads{};
+  std::uint64_t n_good_reads{};
 
   // n_hairpin_reads is the number of read pairs identified as being
   // hairpins using the criteria in the "cutoff" variable.
-  uint64_t n_hairpin_reads{};
+  std::uint64_t n_hairpin_reads{};
 
   // sum_percent_match_good is the sum of the percent matches between
   // the read ends for reads that do not meet the criteria for hairpin.
@@ -182,14 +175,16 @@ struct hp_summary {
   // sum_percent_match_bad over the total hairpin reads.
   double mean_percent_match_hairpin{};
 
-  auto assign_values() -> void {
+  auto
+  assign_values() -> void {
     mean_percent_match_non_hairpin =
       sum_percent_match_good / (n_reads - n_hairpin_reads);
     mean_percent_match_hairpin = sum_percent_match_bad / n_hairpin_reads;
   }
 
-  auto tostring() const -> string {
-    ostringstream oss;
+  auto
+  tostring() const -> std::string {
+    std::ostringstream oss;
     oss << "total_reads_pairs: " << n_reads << '\n'
         << "good_reads_pairs: " << n_good_reads << '\n'
         << "hairpin_read_pairs: " << n_hairpin_reads << '\n'
@@ -204,65 +199,71 @@ struct hp_summary {
 };
 
 static void
-write_histogram(const string &hist_outfile, vector<double> hist) {
-  ofstream hist_out(hist_outfile);
+write_histogram(const std::string &hist_outfile, std::vector<double> hist) {
+  std::ofstream hist_out(hist_outfile);
   if (!hist_out)
-    throw runtime_error("failed to open file: " + hist_outfile);
-  const auto total = accumulate(cbegin(hist), cend(hist), 0.0);
-  transform(cbegin(hist), cend(hist), begin(hist),
-            [&total](const double t) { return t / total; });
+    throw std::runtime_error("failed to open file: " + hist_outfile);
+  const auto total = std::accumulate(std::cbegin(hist), std::cend(hist), 0.0);
+  std::transform(std::cbegin(hist), std::cend(hist), std::begin(hist),
+                 [&](const double t) { return t / total; });
   const double increment = 1.0 / size(hist);
   auto idx = 0;
   hist_out << std::fixed;
   hist_out.precision(3);
+  // NOLINTBEGIN(clang-analyzer-*,cert-flp30-c)
   for (double offset = 0.0; offset < 1.0; offset += increment)
     hist_out << offset << '\t' << hist[idx++] << '\n';
+  // NOLINTEND(clang-analyzer-*,cert-flp30-c)
 }
 
 static void
-write_statistics(const string &filename, hp_summary hps) {
+write_statistics(const std::string &filename, hp_summary hps) {
   hps.assign_values();
-  ofstream out(filename);
+  std::ofstream out(filename);
   if (!out)
-    throw runtime_error("failed to open file: " + filename);
+    throw std::runtime_error("failed to open file: " + filename);
   out << hps.tostring();
 }
 
 static inline double
 fraction_good_bases(const FASTQRecord &a, const FASTQRecord &b) {
-  const double a_bases = count_if(cbegin(a.seq), cend(a.seq), &valid_base);
-  const double b_bases = count_if(cbegin(b.seq), cend(b.seq), &valid_base);
+  const double a_bases =
+    std::count_if(std::cbegin(a.seq), std::cend(a.seq), &valid_base);
+  const double b_bases =
+    std::count_if(std::cbegin(b.seq), std::cend(b.seq), &valid_base);
   return (a_bases + b_bases) / (size(a.seq) + size(b.seq));
 }
 
 struct clean_hairpin {
   double cutoff{0.95};
-  // ADS: this was uint64_t but g++-14.2.0 on macOS had a problem
-  size_t n_reads_to_check{std::numeric_limits<size_t>::max()};
+  // ADS: this was std::uint64_t but g++-14.2.0 on macOS had a problem
+  std::size_t n_reads_to_check{std::numeric_limits<std::size_t>::max()};
   double min_good_base_percent{0.5};
-  uint32_t min_read_length{32};
-  uint32_t n_hist_bins{20};
+  std::uint32_t min_read_length{32};
+  std::uint32_t n_hist_bins{20};
   bool invert_output{false};
 
-  hp_summary analyze_reads(const string &outfile1, const string &outfile2,
-                           bgzf_file &in1, bgzf_file &in2,
-                           vector<double> &hist) const;
-  hp_summary analyze_reads(bgzf_file &in1, bgzf_file &in2,
-                           vector<double> &hist) const;
+  hp_summary
+  analyze_reads(const std::string &outfile1, const std::string &outfile2,
+                bamxx::bgzf_file &in1, bamxx::bgzf_file &in2,
+                std::vector<double> &hist) const;
+  hp_summary
+  analyze_reads(bamxx::bgzf_file &in1, bamxx::bgzf_file &in2,
+                std::vector<double> &hist) const;
 };
 
 hp_summary
-clean_hairpin::analyze_reads(const string &outfile1, const string &outfile2,
-                             bgzf_file &in1, bgzf_file &in2,
-                             vector<double> &hist) const {
-
+clean_hairpin::analyze_reads(const std::string &outfile1,
+                             const std::string &outfile2, bamxx::bgzf_file &in1,
+                             bamxx::bgzf_file &in2,
+                             std::vector<double> &hist) const {
   // output files for read1 and read2 with hairpins removed
-  bgzf_file out1(outfile1, "w");
+  bamxx::bgzf_file out1(outfile1, "w");
   if (!out1)
-    throw runtime_error("cannot open output file: " + outfile1);
-  bgzf_file out2(outfile2, "w");
+    throw std::runtime_error("cannot open output file: " + outfile1);
+  bamxx::bgzf_file out2(outfile2, "w");
   if (!out2)
-    throw runtime_error("cannot open output file: " + outfile2);
+    throw std::runtime_error("cannot open output file: " + outfile2);
 
   hp_summary hps{cutoff};
   FASTQRecord r1, r2;
@@ -280,7 +281,7 @@ clean_hairpin::analyze_reads(const string &outfile1, const string &outfile2,
       similarity_both_bisulfite_conversions(r1.seq, r2.seq);
 
     // ADS: need a bitter way to get this bin identifier
-    ++hist[floor(percent_match * n_hist_bins)];
+    ++hist[std::floor(percent_match * n_hist_bins)];
 
     if (percent_match > cutoff) {
       hps.sum_percent_match_bad += percent_match;
@@ -302,8 +303,8 @@ clean_hairpin::analyze_reads(const string &outfile1, const string &outfile2,
 }
 
 hp_summary
-clean_hairpin::analyze_reads(bgzf_file &in1, bgzf_file &in2,
-                             vector<double> &hist) const {
+clean_hairpin::analyze_reads(bamxx::bgzf_file &in1, bamxx::bgzf_file &in2,
+                             std::vector<double> &hist) const {
   hp_summary hps{cutoff};
   FASTQRecord r1, r2;
   while (hps.n_reads < n_reads_to_check && in1 >> r1 && in2 >> r2) {
@@ -317,7 +318,7 @@ clean_hairpin::analyze_reads(bgzf_file &in1, bgzf_file &in2,
 
     const double percent_match =
       similarity_both_bisulfite_conversions(r1.seq, r2.seq);
-    ++hist[floor(percent_match * n_hist_bins)];
+    ++hist[std::floor(percent_match * n_hist_bins)];
 
     if (percent_match > cutoff) {
       hps.sum_percent_match_bad += percent_match;
@@ -330,24 +331,21 @@ clean_hairpin::analyze_reads(bgzf_file &in1, bgzf_file &in2,
 }
 
 int
-main_clean_hairpins(int argc, char *argv[]) {
-
-  static const string description = "fix and stat invdup/hairpin reads";
-
+main_clean_hairpins(int argc, char *argv[]) {  // NOLINT(*-avoid-c-arrays)
+  static constexpr auto description = "fix and stat invdup/hairpin reads";
   try {
-
-    string outfile1;
-    string outfile2;
-    string stat_outfile;
-    string hist_outfile;
+    std::string outfile1;
+    std::string outfile2;
+    std::string stat_outfile;
+    std::string hist_outfile;
 
     clean_hairpin ch{};
 
     bool verbose = false;
 
     /****************** COMMAND LINE OPTIONS ********************/
-    OptionParser opt_parse(strip_path(argv[0]), description,
-                           "<end1-fastq> <end2-fastq>", true);
+    OptionParser opt_parse(argv[0],  // NOLINT(*-pointer-arithmetic)
+                           description, "<end1-fastq> <end2-fastq>", true);
     opt_parse.set_show_defaults();
     opt_parse.add_opt("out1", 'o', "output file for read 1", false, outfile1);
     opt_parse.add_opt("out2", 'p', "output file for read 2", false, outfile2);
@@ -367,42 +365,43 @@ main_clean_hairpins(int argc, char *argv[]) {
     opt_parse.add_opt("bins", 'b', "number of histograms bins", false,
                       ch.n_hist_bins);
     opt_parse.add_opt("verbose", 'v', "print more run info", false, verbose);
-    vector<string> leftover_args;
+    std::vector<std::string> leftover_args;
     opt_parse.parse(argc, argv, leftover_args);
     if (argc == 1 || opt_parse.help_requested()) {
-      cerr << opt_parse.help_message() << endl
-           << opt_parse.about_message() << endl;
+      std::cerr << opt_parse.help_message() << '\n'
+                << opt_parse.about_message() << '\n';
       return EXIT_SUCCESS;
     }
     if (opt_parse.option_missing()) {
-      cerr << opt_parse.option_missing_message() << endl;
+      std::cerr << opt_parse.option_missing_message() << '\n';
       return EXIT_SUCCESS;
     }
     if (opt_parse.about_requested() || leftover_args.size() != 2) {
-      cerr << opt_parse.about_message() << endl;
+      std::cerr << opt_parse.about_message() << '\n';
       return EXIT_SUCCESS;
     }
-    const string reads_file1(leftover_args.front());
-    const string reads_file2(leftover_args.back());
+    const std::string reads_file1(leftover_args.front());
+    const std::string reads_file2(leftover_args.back());
     /****************** END COMMAND LINE OPTIONS *****************/
 
     if (outfile1.empty() != outfile2.empty()) {
       // ADS: add message about number of reads to check and output files
-      cerr << "error: specify both or neither of out1/o and out2/p" << endl;
+      std::cerr << "error: specify both or neither of out1/o and out2/p"
+                << '\n';
       return EXIT_FAILURE;
     }
 
     const bool write_reads = !outfile1.empty();
 
-    vector<double> hist(ch.n_hist_bins, 0.0);
+    std::vector<double> hist(ch.n_hist_bins, 0.0);
 
     // Input: paired-end reads with end1 and end2
-    bgzf_file in1(reads_file1, "r");
+    bamxx::bgzf_file in1(reads_file1, "r");
     if (!in1)
-      throw runtime_error("cannot open input file: " + reads_file1);
-    bgzf_file in2(reads_file2, "r");
+      throw std::runtime_error("cannot open input file: " + reads_file1);
+    bamxx::bgzf_file in2(reads_file2, "r");
     if (!in2)
-      throw runtime_error("cannot open input file: " + reads_file2);
+      throw std::runtime_error("cannot open input file: " + reads_file2);
 
     const hp_summary hps =
       write_reads ? ch.analyze_reads(outfile1, outfile2, in1, in2, hist)
@@ -415,8 +414,10 @@ main_clean_hairpins(int argc, char *argv[]) {
       write_histogram(hist_outfile, hist);
   }
   catch (const std::exception &e) {
-    cerr << e.what() << endl;
+    std::cerr << e.what() << '\n';
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
 }
+
+// NOLINTEND(*-narrowing-conversions,*-magic-numbers)

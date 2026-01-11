37e68785e9b37c65933c5903cbbf1ac9de5499a2
Andrew D Smith <andrewds@usc.edu>
2026-01-10 17:10:09 -0800

    src/analysis/nanopore.cpp: fixing copyright header

--- src/analysis/nanopore.cpp
+++ src/analysis/nanopore.cpp
@@ -9,6 +9,9 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifdef BUILD_NANOPORE
